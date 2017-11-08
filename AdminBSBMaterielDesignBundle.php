28e3892fb1e6e3521efb340265f438cc032d2bda
webeweb <support@webeweb.fr>
2017-11-08 15:08:31 +0100

    Add main bundle class

---
+++ AdminBSBMaterielDesignBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AdminBSB Material Design bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle
+ * @final
+ */
+final class AdminBSBMaterialDesignBundle extends Bundle {
+
+}
