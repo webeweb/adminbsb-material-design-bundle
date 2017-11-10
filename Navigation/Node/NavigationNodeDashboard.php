0c19cbec684336d0c784114542bc65e9b2353f5a
webeweb <support@webeweb.fr>
2017-11-10 16:40:07 +0100

    Add navigation node "Dashboard"

---
+++ Navigation/Node/NavigationNodeDashboard.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Navigation\Node;
+
+use WBW\Library\Core\Navigation\Node\NavigationNode;
+
+/**
+ * Navigation node "Dashboard".
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Navigation\Node
+ * @final
+ */
+final class NavigationNodeDashboard extends NavigationNode {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $route The route.
+	 */
+	public function __construct($route = null) {
+		parent::__construct("navigation.node.dashboard", "dashboard", $route);
+	}
+
+}
