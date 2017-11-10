767da89ab7089e456e53fef3b3497ded5055d84f
webeweb <support@webeweb.fr>
2017-11-10 16:40:34 +0100

    Add navigation node "Parameters"

---
+++ Tests/Navigation/Node/NavigationNodeParametersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Navigation\Node;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Navigation\Node\NavigationNodeParameters;
+
+/**
+  /**
+ * Navigation node "Parameters" test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Navigation\Node
+ * @final
+ */
+final class NavigationNodeParametersTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new NavigationNodeParameters("route");
+
+		$this->assertEquals("navigation.node.parameters", $obj->getId(), "The method getId() does not return the expecetd value");
+		$this->assertEquals("build", $obj->getIcon(), "The method getIcon() does not return the expecetd value");
+		$this->assertEquals("route", $obj->getRoute(), "The method getRoute() does not return the expecetd value");
+	}
+
+}
