e8b7eeee029666c79904d3c927f3387787406f44
webeweb <support@webeweb.fr>
2017-11-11 13:51:02 +0100

    Add default drop down hook provider

---
+++ Tests/Provider/DropDown/DefaultDropDownHookProviderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\DropDown;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownHookProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownHookProviderInterface;
+
+/**
+ * Default drop down hook provider test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\DropDown
+ * @final
+ */
+final class DefaultDropDownHookProviderTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new DefaultDropDownHookProvider();
+
+		$this->assertEquals([], $obj->getItems(), "The method getItems() does not return the expected value");
+		$this->assertEquals(DropDownHookProviderInterface::DEFAULT_TEMPLATE, $obj->getTemplate(), "The method getTemplate() does not return the expected value");
+	}
+
+}
