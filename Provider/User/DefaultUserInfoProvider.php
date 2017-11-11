1fec840802c30e23a1ece70ee565648c3a2781ff
webeweb <support@webeweb.fr>
2017-11-11 13:50:47 +0100

    Add default user info provider

---
+++ Provider/User/DefaultUserInfoProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User;
+
+/**
+ * Default user info provider.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User
+ * @final
+ */
+final class DefaultUserInfoProvider implements UserInfoProviderInterface {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING TO DO.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTemplate() {
+		return self::DEFAULT_TEMPLATE;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function provideRegisterLink() {
+		return false;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function provideResettingLink() {
+		return false;
+	}
+
+}
---
+++ Tests/Provider/User/DefaultUserInfoProviderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\User;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\DefaultUserInfoProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\UserInfoProviderInterface;
+
+/**
+ * Default user info provider test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\User
+ * @final
+ */
+final class DefaultUserInfoProviderTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new DefaultUserInfoProvider();
+
+		$this->assertEquals(UserInfoProviderInterface::DEFAULT_TEMPLATE, $obj->getTemplate(), "The method getTemplate() does not return the expected value");
+		$this->assertEquals(false, $obj->provideRegisterLink(), "The method provideRegisterLink() does not return the expected value");
+		$this->assertEquals(false, $obj->provideResettingLink(), "The method provideResettingLink() does not return the expected value");
+	}
+
+}
