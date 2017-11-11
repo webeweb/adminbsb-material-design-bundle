15c28bb5b6641e05c5cdc1c7a6a4bd2e7f1bb0a0
webeweb <support@webeweb.fr>
2017-11-11 13:50:03 +0100

    Add default breadcrumbs provider

---
+++ Provider/Breadcrumbs/DefaultBreadcrumbsProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs;
+
+/**
+ * Default breadcrumbs provider.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs
+ * @final
+ */
+final class DefaultBreadcrumbsProvider implements BreadcrumbsProviderInterface {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING TO DO.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTemplate() {
+		return self::DEFAULT_TEMPLATE;
+	}
+
+}
