1b998cf1e12a53cf902ff92102c5d49d748653fa
webeweb <support@webeweb.fr>
2017-11-11 13:50:33 +0100

    Add default search provider

---
+++ Tests/Provider/Search/DefaultSearchProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Search;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\SearchProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\DefaultSearchProvider;
+
+/**
+ * Default search provider test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Search
+ * @final
+ */
+final class DefaultSearchProviderTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new DefaultSearchProvider();
+
+		$this->assertEquals(SearchProviderInterface::DEFAULT_TEMPLATE, $obj->getTemplate(), "The method getTemplate() does not return the expected value");
+	}
+
+}
