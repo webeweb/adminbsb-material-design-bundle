857f22d3cf478cb480718851b4530fafa4b73bf7
webeweb <support@webeweb.fr>
2017-11-20 15:14:13 +0100

    Add radio button and switch button form twig extension

--- Twig/Extension/Form/AbstractFormTwigExtension.php
+++ Twig/Extension/Form/AbstractFormTwigExtension.php
@@ -64,4 +64,72 @@ abstract class AbstractFormTwigExtension extends AbstractABSBMDTwigExtension {
 		return $this->replace($template, ["%attributes%", "%id%", "%content%"], [StringUtility::parseArray($_attr), $_attr["id"], $_content]);
 	}
 
+	/**
+	 * Displays a radio button.
+	 *
+	 * @param string $content The radio button content.
+	 * @param string $name The radio button name.
+	 * @param string $id The radio button id.
+	 * @param boolean $checked Checked ?
+	 * @param boolean $disabled Disabled ?
+	 * @param boolean $withGap Filled in ?
+	 * @param string $class The radio button class.
+	 * @return string Returns the radio button.
+	 */
+	protected final function radioButton($content, $name, $id, $checked, $disabled, $withGap, $class) {
+
+		// Initialize the template.
+		$template = '<input %attributes%><label for="%id%">%content%</label>';
+
+		// Initialize the attributes.
+		$_attr = [];
+
+		$_attr["class"]		 = [$withGap === true ? "with-gap" : null, $class];
+		$_attr["name"]		 = $name;
+		$_attr["type"]		 = "radio";
+		$_attr["id"]		 = $id;
+		$_attr["checked"]	 = $checked === true ? "checked" : null;
+		$_attr["disabled"]	 = $disabled === true ? "disabled" : null;
+
+		// Check the parameters.
+		$_content = !is_null($content) ? $content : self::DEFAULT_CONTENT;
+
+		// Return the HTML.
+		return $this->replace($template, ["%attributes%", "%id%", "%content%"], [StringUtility::parseArray($_attr), $_attr["id"], $_content]);
+	}
+
+	/**
+	 * Displays a switch button.
+	 *
+	 * @param string $offLabel The switch button off label.
+	 * @param string $name The switch button name.
+	 * @param boolean $checked Checked switch button ?
+	 * @param boolean $disabled Disable switch button ?
+	 * @param string $onLabel The switch button on label.
+	 * @param array $attr The switch button attributes.
+	 * @param string $class The switch button class.
+	 * @return string Returns the switch button.
+	 */
+	protected final function switchButton($offLabel, $name, $checked, $disabled, $onLabel, array $attr, $class = null) {
+
+		// Initialize the template.
+		$template = '<div class="switch"><label>%offLabel%<input %attributes%><span class="lever%class%"></span>%onLabel%</label></div>';
+
+		// Initialize the attributes.
+		$_attr = $attr;
+
+		$_attr["name"]		 = $name;
+		$_attr["type"]		 = "checkbox";
+		$_attr["checked"]	 = $checked === true ? "checked" : null;
+		$_attr["disabled"]	 = $disabled === true ? "disabled" : null;
+
+		// Check the parameters.
+		$_offLabel	 = !is_null($offLabel) ? $offLabel : "";
+		$_class		 = !is_null($class) ? ' ' . trim($class) : "";
+		$_onLabel	 = !is_null($onLabel) ? $onLabel : "";
+
+		// Return the HTML.
+		return $this->replace($template, ["%offLabel%", "%attributes%", "%class%", "%onLabel%"], [$_offLabel, StringUtility::parseArray($_attr), $_class, $_onLabel]);
+	}
+
 }
