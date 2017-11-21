6aa239e1b593fa930f91fbed3ab26fb0ae27dbb4
webeweb <support@webeweb.fr>
2017-11-21 10:29:00 +0100

    Rename argument

--- Twig/Extension/Form/AbstractFormTwigExtension.php
+++ Twig/Extension/Form/AbstractFormTwigExtension.php
@@ -106,17 +106,17 @@ abstract class AbstractFormTwigExtension extends AbstractABSBMDTwigExtension {
 	 * @param boolean $checked Checked switch button ?
 	 * @param boolean $disabled Disable switch button ?
 	 * @param string $onLabel The switch button on label.
-	 * @param array $attr The switch button attributes.
+	 * @param array $attributes The switch button attributes.
 	 * @param string $class The switch button class.
 	 * @return string Returns the switch button.
 	 */
-	protected final function switchButton($offLabel, $name, $checked, $disabled, $onLabel, array $attr, $class = null) {
+	protected final function switchButton($offLabel, $name, $checked, $disabled, $onLabel, array $attributes, $class = null) {
 
 		// Initialize the template.
 		$template = '<div class="switch"><label>%offLabel%<input %attributes%><span class="lever%class%"></span>%onLabel%</label></div>';
 
 		// Initialize the attributes.
-		$_attr = $attr;
+		$_attr = $attributes;
 
 		$_attr["name"]		 = $name;
 		$_attr["type"]		 = "checkbox";
@@ -125,7 +125,7 @@ abstract class AbstractFormTwigExtension extends AbstractABSBMDTwigExtension {
 
 		// Check the parameters.
 		$_offLabel	 = !is_null($offLabel) ? $offLabel : "";
-		$_class		 = !is_null($class) ? ' ' . trim($class) : "";
+		$_class		 = !is_null($class) ? " " . trim($class) : "";
 		$_onLabel	 = !is_null($onLabel) ? $onLabel : "";
 
 		// Return the HTML.
