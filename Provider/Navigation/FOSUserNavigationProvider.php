2820edd325239712bf0e72250fa30fa4e990ade4
webeweb <support@webeweb.fr>
2017-12-08 15:58:46 +0100

    Add FOS User navigation provider

---
+++ Provider/Navigation/FOSUserNavigationProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Navigation;
+
+use WBW\Library\Core\Navigation\Node\BreadcrumbNode;
+
+/**
+ * FOSUserNavigationProvider.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Navigation
+ * @final
+ */
+final class FOSUserNavigationProvider {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING TO DO.
+	}
+
+	/**
+	 * Get the breadcrumb.
+	 *
+	 * @return BreadcrumbNode Returns the breadcrumb.
+	 */
+	public function getBreadcrumb() {
+
+		// Initialize the breadcrumb.
+		$breadcrumb = new BreadcrumbNode("label.user", "person");
+		$breadcrumb->addNode(new BreadcrumbNode("label.user_profile_edit", "person", "fos_user_profile_edit"));
+		$breadcrumb->addNode(new BreadcrumbNode("label.user_change_password", "person", "fos_user_change_password"));
+
+		// Return the breadcrumb.
+		return $breadcrumb;
+	}
+
+}
---
+++ Tests/Provider/Navigation/FOSUserNavigationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Navigation;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Navigation\FOSUserNavigationProvider;
+use WBW\Library\Core\Navigation\Node\BreadcrumbNode;
+
+/**
+ * FOS User navigation provider test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Navigation
+ * @final
+ */
+final class FOSUserNavigationProviderTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the getBreadcrumb() method.
+	 *
+	 * @return void
+	 */
+	public function testGetBreadcrumb() {
+
+		$obj = new FOSUserNavigationProvider();
+
+		$res = $obj->getBreadcrumb();
+		$this->assertInstanceOf(BreadcrumbNode::class, $res);
+		$this->assertCount(2, $res->getNodes());
+		$this->assertEquals("label.user_profile_edit", $res->getNodeAt(0)->getId());
+		$this->assertEquals("person", $res->getNodeAt(0)->getIcon());
+		$this->assertEquals("fos_user_profile_edit", $res->getNodeAt(0)->getRoute());
+		$this->assertEquals("label.user_change_password", $res->getNodeAt(1)->getId());
+		$this->assertEquals("person", $res->getNodeAt(1)->getIcon());
+		$this->assertEquals("fos_user_change_password", $res->getNodeAt(1)->getRoute());
+	}
+
+}
