9145ba0bc9c580f92471d5e2dd1968b5c7ef96ae
webeweb <support@webeweb.fr>
2017-12-11 09:40:26 +0100

    Add Providers setter

---
+++ Provider/Setter/ProvidersSetter.php
@@ -0,0 +1,278 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Setter;
+
+use Twig_Environment;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Application\ApplicationProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Application\DefaultApplicationProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs\BreadcrumbsProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs\DefaultBreadcrumbsProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownHookProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownNotificationsProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownTasksProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownHookProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownNotificationsProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownTasksProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Footer\DefaultFooterProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Footer\FooterProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\DefaultSearchProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\SearchProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\DefaultUserInfoProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\UserInfoProviderInterface;
+
+/**
+ * Providers setter.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Setter
+ * @final
+ */
+final class ProvidersSetter {
+
+	/**
+	 * Service name.
+	 */
+	const SERVICE_NAME = "webeweb.bundle.adminbsb-materialdesignbundle.provider.setter";
+
+	/**
+	 * Application provider.
+	 *
+	 * @var ApplicationProviderInterface
+	 */
+	private $applicationProvider;
+
+	/**
+	 * Breadcrumbs provider.
+	 *
+	 * @var BreadcrumbsProviderInterface
+	 */
+	private $breadcrumbsProvider;
+
+	/**
+	 * Drop down hook provider.
+	 *
+	 * @var DropDownHookProviderInterface
+	 */
+	private $dropDownHookProvider;
+
+	/**
+	 * Drop down notifications provider.
+	 *
+	 * @var DropDownNotificationsProviderInterface
+	 */
+	private $dropDownNotificationsProvider;
+
+	/**
+	 * Drop down tasks provider.
+	 *
+	 * @var DropDownTasksProviderInterface
+	 */
+	private $dropDownTasksProvider;
+
+	/**
+	 * Footer provider.
+	 *
+	 * @var FooterProviderInterface
+	 */
+	private $footerProvider;
+
+	/**
+	 * Search provider.
+	 *
+	 * @var SearchProviderInterface
+	 */
+	private $searchProvider;
+
+	/**
+	 * Twig service.
+	 *
+	 * @var Twig_Environment
+	 */
+	private $twig;
+
+	/**
+	 * User info provider.
+	 *
+	 * @var UserInfoProviderInterface
+	 */
+	private $userInfoProvider;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Twig_Environment $twig The Twig service.
+	 */
+	public function __construct(Twig_Environment $twig) {
+		$this->twig = $twig;
+	}
+
+	/**
+	 * Get the application provider.
+	 *
+	 * @return ApplicationProviderInterface Returns the application provider.
+	 */
+	public function getApplicationProvider() {
+		return !is_null($this->applicationProvider) ? $this->applicationProvider : new DefaultApplicationProvider();
+	}
+
+	/**
+	 * Get the breadcrumbs provider.
+	 *
+	 * @return BreadcrumbsProviderInterface Returns the breadcrumbs provider.
+	 */
+	public function getBreadcrumbsProvider() {
+		return !is_null($this->breadcrumbsProvider) ? $this->breadcrumbsProvider : new DefaultBreadcrumbsProvider();
+	}
+
+	/**
+	 * Get the drop down hook provider.
+	 *
+	 * @return DropDownHookProviderInterface Returns the drop down hook provider.
+	 */
+	public function getDropDownHookProvider() {
+		return !is_null($this->dropDownHookProvider) ? $this->dropDownHookProvider : new DefaultDropDownHookProvider();
+	}
+
+	/**
+	 * Get the drop down notifications provider.
+	 *
+	 * @return DropDownNotificationsProviderInterface Returns the drop down notifications provider.
+	 */
+	public function getDropDownNotificationsProvider() {
+		return !is_null($this->dropDownNotificationsProvider) ? $this->dropDownNotificationsProvider : new DefaultDropDownNotificationsProvider();
+	}
+
+	/**
+	 * Get the drop down tasks provider.
+	 *
+	 * @return DropDownTasksProviderInterface Returns the drop down tasks provider.
+	 */
+	public function getDropDownTasksProvider() {
+		return !is_null($this->dropDownTasksProvider) ? $this->dropDownTasksProvider : new DefaultDropDownTasksProvider();
+	}
+
+	/**
+	 * Get the footer provider.
+	 *
+	 * @return FooterProviderInterface Returns the footer provider.
+	 */
+	public function getFooterProvider() {
+		return !is_null($this->footerProvider) ? $this->footerProvider : new DefaultFooterProvider();
+	}
+
+	/**
+	 * Get the search provider.
+	 *
+	 * @return SearchProviderInterface Returns the search provider.
+	 */
+	public function getSearchProvider() {
+		return !is_null($this->searchProvider) ? $this->searchProvider : new DefaultSearchProvider();
+	}
+
+	/**
+	 * Get the user info provider.
+	 *
+	 * @return UserInfoProviderInterface Returns the user info provider.
+	 */
+	public function getUserInfoProvider() {
+		return !is_null($this->userInfoProvider) ? $this->userInfoProvider : new DefaultUserInfoProvider();
+	}
+
+	/**
+	 * Set the application provider.
+	 *
+	 * @param ApplicationProviderInterface $applicationProvider The application provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setApplicationProvider(ApplicationProviderInterface $applicationProvider) {
+		$this->applicationProvider = $applicationProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the breadcrumbs provider.
+	 *
+	 * @param BreadcrumbsProviderInterface $breadcrumbsProvider The breadcrumbs provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setBreadcrumbsProvider(BreadcrumbsProviderInterface $breadcrumbsProvider) {
+		$this->breadcrumbsProvider = $breadcrumbsProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the drop down hook provider.
+	 *
+	 * @param DropDownHookProviderInterface $dropDownHookProvider The drop down hook provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setDropDownHookProvider(DropDownHookProviderInterface $dropDownHookProvider) {
+		$this->dropDownHookProvider = $dropDownHookProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the drop down notifications provider.
+	 *
+	 * @param DropDownNotificationsProviderInterface $dropDownNotificationsProvider The drop down notifications provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setDropDownNotificationsProvider(DropDownNotificationsProviderInterface $dropDownNotificationsProvider) {
+		$this->dropDownNotificationsProvider = $dropDownNotificationsProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the drop down tasks provider.
+	 *
+	 * @param DropDownTasksProviderInterface $dropDownTasksProvider The drop down task provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setDropDownTasksProvider(DropDownTasksProviderInterface $dropDownTasksProvider) {
+		$this->dropDownTasksProvider = $dropDownTasksProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the footer provider.
+	 *
+	 * @param FooterProviderInterface $footerProvider The footer priovider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setFooterProvider(FooterProviderInterface $footerProvider) {
+		$this->footerProvider = $footerProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the search provider.
+	 *
+	 * @param SearchProviderInterface $searchProvider The search provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setSearchProvider(SearchProviderInterface $searchProvider) {
+		$this->searchProvider = $searchProvider;
+		return $this;
+	}
+
+	/**
+	 * Set the user info provider.
+	 *
+	 * @param UserInfoProviderInterface $userInfoProvider The user info provider.
+	 * @return ProvidersSetter Returns the providers setter.
+	 */
+	public function setUserInfoProvider(UserInfoProviderInterface $userInfoProvider) {
+		$this->userInfoProvider = $userInfoProvider;
+		return $this;
+	}
+
+}
---
+++ Tests/Provider/Setter/ProvidersSetterTest.php
@@ -0,0 +1,214 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Setter;
+
+use PHPUnit_Framework_TestCase;
+use Twig_Environment;
+use Twig_LoaderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Application\ApplicationProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Application\DefaultApplicationProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs\BreadcrumbsProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Breadcrumbs\DefaultBreadcrumbsProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownHookProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownNotificationsProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DefaultDropDownTasksProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownHookProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownNotificationsProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\DropDown\DropDownTasksProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Footer\DefaultFooterProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Footer\FooterProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\DefaultSearchProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Search\SearchProviderInterface;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\Setter\ProvidersSetter;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\DefaultUserInfoProvider;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Provider\User\UserInfoProviderInterface;
+
+/**
+ * Providers setter test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Provider\Setter
+ * @final
+ */
+final class ProvidersSetterTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Twiog environment.
+	 *
+	 * @var Twig_Environment
+	 */
+	private $twigEnvironment;
+
+	/**
+	 * Twig loader.
+	 *
+	 * @var Twig_Loader
+	 */
+	private $twigLoader;
+
+	/**
+	 * Set up.
+	 *
+	 * @return void.
+	 */
+	public function setUp() {
+		$this->twigLoader		 = $this->getMockBuilder(Twig_LoaderInterface::class)->getMock();
+		$this->twigEnvironment	 = $this->getMockBuilder(Twig_Environment::class)->setConstructorArgs([$this->twigLoader, []])->getMock();
+	}
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$this->assertInstanceOf(DefaultApplicationProvider::class, $obj->getApplicationProvider());
+		$this->assertInstanceOf(DefaultBreadcrumbsProvider::class, $obj->getBreadcrumbsProvider());
+		$this->assertInstanceOf(DefaultDropDownHookProvider::class, $obj->getDropDownHookProvider());
+		$this->assertInstanceOf(DefaultDropDownNotificationsProvider::class, $obj->getDropDownNotificationsProvider());
+		$this->assertInstanceOf(DefaultDropDownTasksProvider::class, $obj->getDropDownTasksProvider());
+		$this->assertInstanceOf(DefaultFooterProvider::class, $obj->getFooterProvider());
+		$this->assertInstanceOf(DefaultSearchProvider::class, $obj->getSearchProvider());
+		$this->assertInstanceOf(DefaultUserInfoProvider::class, $obj->getUserInfoProvider());
+	}
+
+	/**
+	 * Tests the setApplicationProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetApplicationProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(ApplicationProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setApplicationProvider($provider);
+		$this->assertEquals($provider, $obj->getApplicationProvider());
+	}
+
+	/**
+	 * Tests the setBreadcrumbsProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetBreadcrumbsProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(BreadcrumbsProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setBreadcrumbsProvider($provider);
+		$this->assertEquals($provider, $obj->getBreadcrumbsProvider());
+	}
+
+	/**
+	 * Tests the setDropDownHookProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetDropDownHookProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(DropDownHookProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setDropDownHookProvider($provider);
+		$this->assertEquals($provider, $obj->getDropDownHookProvider());
+	}
+
+	/**
+	 * Tests the setDropDownNotificationsProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetDropDownNotificationsProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(DropDownNotificationsProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setDropDownNotificationsProvider($provider);
+		$this->assertEquals($provider, $obj->getDropDownNotificationsProvider());
+	}
+
+	/**
+	 * Tests the setDropDownTasksProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetDropDownTasksProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(DropDownTasksProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setDropDownTasksProvider($provider);
+		$this->assertEquals($provider, $obj->getDropDownTasksProvider());
+	}
+
+	/**
+	 * Tests the setFooterProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetFooterProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(FooterProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setFooterProvider($provider);
+		$this->assertEquals($provider, $obj->getFooterProvider());
+	}
+
+	/**
+	 * Tests the setSearchProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetSearchProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(SearchProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setSearchProvider($provider);
+		$this->assertEquals($provider, $obj->getSearchProvider());
+	}
+
+	/**
+	 * Tests the setUserInfoProvider() method.
+	 *
+	 * @return void
+	 * @depends testConstructor
+	 */
+	public function testSetUserInfoProvider() {
+
+		// Set the mocks.
+		$provider = $this->getMockBuilder(UserInfoProviderInterface::class)->getMock();
+
+		$obj = new ProvidersSetter($this->twigEnvironment);
+		$obj->setUserInfoProvider($provider);
+		$this->assertEquals($provider, $obj->getUserInfoProvider());
+	}
+
+}
