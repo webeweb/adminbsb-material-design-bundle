3fda6bbd9a4ae32b569844fa9f6ac872133f1fb6
webeweb <support@webeweb.fr>
2018-03-16 13:26:13 +0100

    AdminBSB alert UI Twig extension are now based on Bootstrap alert Twig extension

--- Tests/Twig/Extension/UI/AlertUITwigExtensionTest.php
+++ Tests/Twig/Extension/UI/AlertUITwigExtensionTest.php
@@ -39,94 +39,94 @@ final class AlertUITwigExtensionTest extends PHPUnit_Framework_TestCase {
         $this->assertCount(6, $res);
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
-        $this->assertEquals("basicAlertDanger", $res[0]->getName());
-        $this->assertEquals([$obj, "basicAlertDangerFunction"], $res[0]->getCallable());
+        $this->assertEquals("absbmdBasicAlertDanger", $res[0]->getName());
+        $this->assertEquals([$obj, "absbmdBasicAlertDangerFunction"], $res[0]->getCallable());
         $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
-        $this->assertEquals("basicAlertInfo", $res[1]->getName());
-        $this->assertEquals([$obj, "basicAlertInfoFunction"], $res[1]->getCallable());
+        $this->assertEquals("absbmdBasicAlertInfo", $res[1]->getName());
+        $this->assertEquals([$obj, "absbmdBasicAlertInfoFunction"], $res[1]->getCallable());
         $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
-        $this->assertEquals("basicAlertSuccess", $res[2]->getName());
-        $this->assertEquals([$obj, "basicAlertSuccessFunction"], $res[2]->getCallable());
+        $this->assertEquals("absbmdBasicAlertSuccess", $res[2]->getName());
+        $this->assertEquals([$obj, "absbmdBasicAlertSuccessFunction"], $res[2]->getCallable());
         $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
-        $this->assertEquals("basicAlertWarning", $res[3]->getName());
-        $this->assertEquals([$obj, "basicAlertWarningFunction"], $res[3]->getCallable());
+        $this->assertEquals("absbmdBasicAlertWarning", $res[3]->getName());
+        $this->assertEquals([$obj, "absbmdBasicAlertWarningFunction"], $res[3]->getCallable());
         $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
-        $this->assertEquals("linkAlert", $res[4]->getName());
-        $this->assertEquals([$obj, "linkAlertFunction"], $res[4]->getCallable());
+        $this->assertEquals("absbmdLinkAlert", $res[4]->getName());
+        $this->assertEquals([$obj, "absbmdLinkAlertFunction"], $res[4]->getCallable());
         $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[5]);
-        $this->assertEquals("materialDesignAlert", $res[5]->getName());
-        $this->assertEquals([$obj, "materialDesignAlertFunction"], $res[5]->getCallable());
+        $this->assertEquals("absbmdMaterialDesignAlert", $res[5]->getName());
+        $this->assertEquals([$obj, "absbmdMaterialDesignAlertFunction"], $res[5]->getCallable());
         $this->assertEquals(["html"], $res[5]->getSafe(new Twig_Node()));
     }
 
     /**
-     * Tests the basicAlertDangerFunction() method.
+     * Tests the absbmdBasicAlertDangerFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testBasicAlertDangerFunction() {
+    public function testAbsbmdBasicAlertDangerFunction() {
 
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert alert-danger">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->basicAlertDangerFunction($arg0));
+        $res0 = '<div class="alert alert-danger" role="alert"></div>';
+        $this->assertEquals($res0, $obj->absbmdBasicAlertDangerFunction($arg0));
     }
 
     /**
-     * Tests the basicAlertInfoFunction() method.
+     * Tests the absbmdBasicAlertInfoFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testBasicAlertInfoFunction() {
+    public function testAbsbmdBasicAlertInfoFunction() {
 
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert alert-info">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->basicAlertInfoFunction($arg0));
+        $res0 = '<div class="alert alert-info" role="alert"></div>';
+        $this->assertEquals($res0, $obj->absbmdBasicAlertInfoFunction($arg0));
     }
 
     /**
-     * Tests the basicAlertSuccessFunction() method.
+     * Tests the absbmdBasicAlertSuccessFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testBasicAlertSuccessFunction() {
+    public function testAbsbmdBasicAlertSuccessFunction() {
 
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert alert-success">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->basicAlertSuccessFunction($arg0));
+        $res0 = '<div class="alert alert-success" role="alert"></div>';
+        $this->assertEquals($res0, $obj->absbmdBasicAlertSuccessFunction($arg0));
     }
 
     /**
-     * Tests the basicAlertWarningFunction() method.
+     * Tests the absbmdBasicAlertWarningFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testBasicAlertWarningFunction() {
+    public function testAbsbmdBasicAlertWarningFunction() {
 
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert alert-warning">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->basicAlertWarningFunction($arg0));
+        $res0 = '<div class="alert alert-warning" role="alert"></div>';
+        $this->assertEquals($res0, $obj->absbmdBasicAlertWarningFunction($arg0));
     }
 
     /**
@@ -140,28 +140,20 @@ final class AlertUITwigExtensionTest extends PHPUnit_Framework_TestCase {
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert bg-red">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->linkAlertFunction($arg0));
+        $res0 = '<a href="javascript:void(0);"></a>';
+        $this->assertEquals($res0, $obj->absbmdLinkAlertFunction($arg0));
 
-        $arg1 = ["content" => "content"];
-        $res1 = '<div class="alert bg-red">content</div>';
-        $this->assertEquals($res1, $obj->linkAlertFunction($arg1));
+        $arg1 = ["href" => "https://github.com/"];
+        $res1 = '<a href="https://github.com/"></a>';
+        $this->assertEquals($res1, $obj->absbmdLinkAlertFunction($arg1));
 
-        $arg2 = ["dismissible" => true];
-        $res2 = '<div class="alert bg-red alert-dismissible"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>&nbsp;</div>';
-        $this->assertEquals($res2, $obj->linkAlertFunction($arg2));
+        $arg2 = ["content" => "content"];
+        $res2 = '<a href="javascript:void(0);">content</a>';
+        $this->assertEquals($res2, $obj->absbmdLinkAlertFunction($arg2));
 
-        $arg3 = ["color" => "exception"];
-        $res3 = '<div class="alert bg-red">&nbsp;</div>';
-        $this->assertEquals($res3, $obj->linkAlertFunction($arg3));
-
-        $arg4 = ["color" => "black"];
-        $res4 = '<div class="alert bg-black">&nbsp;</div>';
-        $this->assertEquals($res4, $obj->linkAlertFunction($arg4));
-
-        $arg9 = ["content" => "content", "dismissible" => true, "color" => "black"];
-        $res9 = '<div class="alert bg-black alert-dismissible"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>content</div>';
-        $this->assertEquals($res9, $obj->linkAlertFunction($arg9));
+        $arg9 = ["href" => "https://github.com/", "content" => "content"];
+        $res9 = '<a href="https://github.com/">content</a>';
+        $this->assertEquals($res9, $obj->absbmdLinkAlertFunction($arg9));
     }
 
     /**
@@ -175,28 +167,28 @@ final class AlertUITwigExtensionTest extends PHPUnit_Framework_TestCase {
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<div class="alert bg-red">&nbsp;</div>';
-        $this->assertEquals($res0, $obj->materialDesignAlertFunction($arg0));
+        $res0 = '<div class="alert bg-red" role="alert"></div>';
+        $this->assertEquals($res0, $obj->absbmdMaterialDesignAlertFunction($arg0));
 
         $arg1 = ["content" => "content"];
-        $res1 = '<div class="alert bg-red">content</div>';
-        $this->assertEquals($res1, $obj->materialDesignAlertFunction($arg1));
+        $res1 = '<div class="alert bg-red" role="alert">content</div>';
+        $this->assertEquals($res1, $obj->absbmdMaterialDesignAlertFunction($arg1));
 
         $arg2 = ["dismissible" => true];
-        $res2 = '<div class="alert bg-red alert-dismissible"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>&nbsp;</div>';
-        $this->assertEquals($res2, $obj->materialDesignAlertFunction($arg2));
+        $res2 = '<div class="alert bg-red alert-dismissible" role="alert"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+        $this->assertEquals($res2, $obj->absbmdMaterialDesignAlertFunction($arg2));
 
         $arg3 = ["color" => "exception"];
-        $res3 = '<div class="alert bg-red">&nbsp;</div>';
-        $this->assertEquals($res3, $obj->materialDesignAlertFunction($arg3));
+        $res3 = '<div class="alert bg-red" role="alert"></div>';
+        $this->assertEquals($res3, $obj->absbmdMaterialDesignAlertFunction($arg3));
 
         $arg4 = ["color" => "black"];
-        $res4 = '<div class="alert bg-black">&nbsp;</div>';
-        $this->assertEquals($res4, $obj->materialDesignAlertFunction($arg4));
+        $res4 = '<div class="alert bg-black" role="alert"></div>';
+        $this->assertEquals($res4, $obj->absbmdMaterialDesignAlertFunction($arg4));
 
         $arg5 = ["content" => "content", "dismissible" => true, "color" => "black"];
-        $res5 = '<div class="alert bg-black alert-dismissible"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>content</div>';
-        $this->assertEquals($res5, $obj->materialDesignAlertFunction($arg5));
+        $res5 = '<div class="alert bg-black alert-dismissible" role="alert"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>content</div>';
+        $this->assertEquals($res5, $obj->absbmdMaterialDesignAlertFunction($arg5));
     }
 
 }
