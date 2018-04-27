2e69cc43491e1b52fd69aec63120cc78ea199512
webeweb <support@webeweb.fr>
2018-04-27 13:39:55 +0200

    Alias Bootstrap strike through typgraphy Twig extension

---
+++ Twig/Extension/Typography/StrikeThroughTypographyTwigExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\Typography;
+
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\StrikeThroughTypographyTwigExtension as BaseTypographyTwigExtension;
+
+/**
+ * Strike through typography Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\Typography
+ */
+class StrikeThroughTypographyTwigExtension extends BaseTypographyTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.typography.strikethrough";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("absbmdStrikeThrough", [$this, "bootstrapStrikeThroughFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
