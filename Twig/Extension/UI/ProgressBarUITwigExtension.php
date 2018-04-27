aabd8fccfc1ee165b508643ef576d58ad5e445b8
webeweb <support@webeweb.fr>
2018-04-27 15:25:10 +0200

    Alias Bootstrap progress bar Twig extension

--- Twig/Extension/UI/ProgressBarUITwigExtension.php
+++ Twig/Extension/UI/ProgressBarUITwigExtension.php
@@ -9,34 +9,43 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI;
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\UI;
 
 use Twig_SimpleFunction;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\AbstractABSBMDTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ProgressBarComponentTwigExtension as BaseUITwigExtension;
 use WBW\Library\Core\Utility\Argument\ArrayUtility;
 
 /**
  * Progress bar UI Twig extension.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\UI
  */
-class ProgressBarUITwigExtension extends AbstractUITwigExtension {
+class ProgressBarUITwigExtension extends BaseUITwigExtension {
 
     /**
      * Service name.
      *
      * @var string
      */
-    const SERVICE_NAME = "webeweb.bundle.adminbsbmaterialdesignbundle.twig.extension.ui.progressbar";
+    const SERVICE_NAME = "webeweb.bundle.adminbsbbundle.twig.extension.ui.progressbar";
 
     /**
-     * Displays a basic progress bar.
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Displays an AdminBSB material design progress bar.
      *
      * @param array $args The arguments.
-     * @return string Returns the basic progress bar.
+     * @return string Returns the AdminBSB material design progress bar.
      */
-    public function basicProgressBarFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false));
+    public function adminBSBMaterialDesignProgressBarFunction(array $args = []) {
+        return $this->bootstrapProgressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), AbstractABSBMDTwigExtension::fixColor(ArrayUtility::get($args, "color", "red"), "bg-"));
     }
 
     /**
@@ -46,63 +55,13 @@ class ProgressBarUITwigExtension extends AbstractUITwigExtension {
      */
     public function getFunctions() {
         return [
-            new Twig_SimpleFunction("basicProgressBar", [$this, "basicProgressBarFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("progressBarDanger", [$this, "progressBarDangerFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("progressBarInfo", [$this, "progressBarInfoFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("progressBarSuccess", [$this, "progressBarSuccessFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("progressBarWarning", [$this, "progressBarWarningFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("materialDesignProgressBar", [$this, "materialDesignProgressBarFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBBasicProgressBar", [$this, "bootstrapBasicProgressBarFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBProgressBarDanger", [$this, "bootstrapProgressBarDangerFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBProgressBarInfo", [$this, "bootstrapProgressBarInfoFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBProgressBarSuccess", [$this, "bootstrapProgressBarSuccessFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBProgressBarWarning", [$this, "bootstrapProgressBarWarningFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBMaterialDesignProgressBar", [$this, "adminBSBMaterialDesignProgressBarFunction"], ["is_safe" => ["html"]]),
         ];
-    }
-
-    /**
-     * Displays a material design progress bar.
-     *
-     * @param array $args The arguments.
-     * @return string Returns the material design progress bar.
-     */
-    public function materialDesignProgressBarFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), $this->getColor(ArrayUtility::get($args, "color", "red"), "bg-"));
-    }
-
-    /**
-     * Displays a progress bar "Danger".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the progress bar "Danger".
-     */
-    public function progressBarDangerFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), "progress-bar-danger");
-    }
-
-    /**
-     * Displays a progress bar "Info".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the progress bar "Info".
-     */
-    public function progressBarInfoFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), "progress-bar-info");
-    }
-
-    /**
-     * Displays a progress bar "Success".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the progress bar "Success".
-     */
-    public function progressBarSuccessFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), "progress-bar-success");
-    }
-
-    /**
-     * Displays a progress bar "Warning".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the progress bar "Warning".
-     */
-    public function progressBarWarningFunction(array $args = []) {
-        return $this->progressBar(ArrayUtility::get($args, "content"), ArrayUtility::get($args, "value", 50), ArrayUtility::get($args, "min", 0), ArrayUtility::get($args, "max", 100), ArrayUtility::get($args, "striped", false), ArrayUtility::get($args, "animated", false), "progress-bar-warning");
     }
 
 }
