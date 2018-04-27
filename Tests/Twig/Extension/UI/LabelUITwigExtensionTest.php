83d41044849501d6b738a39568a869053269812a
webeweb <support@webeweb.fr>
2018-04-27 15:39:52 +0200

    Alias Bootstrap label Twig extension

--- Tests/Twig/Extension/UI/LabelUITwigExtensionTest.php
+++ Tests/Twig/Extension/UI/LabelUITwigExtensionTest.php
@@ -9,18 +9,18 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\UI;
+namespace WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI;
 
 use PHPUnit_Framework_TestCase;
 use Twig_Node;
 use Twig_SimpleFunction;
-use WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI\LabelUITwigExtension;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\LabelUITwigExtension;
 
 /**
  * Label UI Twig extension test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\UI
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI
  * @final
  */
 final class LabelUITwigExtensionTest extends PHPUnit_Framework_TestCase {
@@ -39,156 +39,66 @@ final class LabelUITwigExtensionTest extends PHPUnit_Framework_TestCase {
         $this->assertCount(7, $res);
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
-        $this->assertEquals("labelDanger", $res[0]->getName());
-        $this->assertEquals([$obj, "labelDangerFunction"], $res[0]->getCallable());
+        $this->assertEquals("adminBSBLabelDanger", $res[0]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelDangerFunction"], $res[0]->getCallable());
         $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
-        $this->assertEquals("labelDefault", $res[1]->getName());
-        $this->assertEquals([$obj, "labelDefaultFunction"], $res[1]->getCallable());
+        $this->assertEquals("adminBSBLabelDefault", $res[1]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelDefaultFunction"], $res[1]->getCallable());
         $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
-        $this->assertEquals("labelInfo", $res[2]->getName());
-        $this->assertEquals([$obj, "labelInfoFunction"], $res[2]->getCallable());
+        $this->assertEquals("adminBSBLabelInfo", $res[2]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelInfoFunction"], $res[2]->getCallable());
         $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
-        $this->assertEquals("labelPrimary", $res[3]->getName());
-        $this->assertEquals([$obj, "labelPrimaryFunction"], $res[3]->getCallable());
+        $this->assertEquals("adminBSBLabelPrimary", $res[3]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelPrimaryFunction"], $res[3]->getCallable());
         $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
-        $this->assertEquals("labelSuccess", $res[4]->getName());
-        $this->assertEquals([$obj, "labelSuccessFunction"], $res[4]->getCallable());
+        $this->assertEquals("adminBSBLabelSuccess", $res[4]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelSuccessFunction"], $res[4]->getCallable());
         $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[5]);
-        $this->assertEquals("labelWarning", $res[5]->getName());
-        $this->assertEquals([$obj, "labelWarningFunction"], $res[5]->getCallable());
+        $this->assertEquals("adminBSBLabelWarning", $res[5]->getName());
+        $this->assertEquals([$obj, "bootstrapLabelWarningFunction"], $res[5]->getCallable());
         $this->assertEquals(["html"], $res[5]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[6]);
-        $this->assertEquals("materialDesignLabel", $res[6]->getName());
-        $this->assertEquals([$obj, "materialDesignLabelFunction"], $res[6]->getCallable());
+        $this->assertEquals("adminBSBMaterialDesignLabel", $res[6]->getName());
+        $this->assertEquals([$obj, "adminBSBMaterialDesignLabelFunction"], $res[6]->getCallable());
         $this->assertEquals(["html"], $res[6]->getSafe(new Twig_Node()));
     }
 
     /**
-     * Tests the labelDangerFunction() method.
+     * Tests the adminBSBMaterialDesignLabelFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testLabelDangerFunction() {
+    public function testAdminBSBMaterialDesignLabelFunction() {
 
         $obj = new LabelUITwigExtension();
 
         $arg0 = [];
-        $res0 = '<span class="label label-danger">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelDangerFunction($arg0));
-    }
-
-    /**
-     * Tests the labelDefaultFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testLabelDefaultFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label label-default">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelDefaultFunction($arg0));
-    }
-
-    /**
-     * Tests the labelInfoFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testLabelInfoFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label label-info">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelInfoFunction($arg0));
-    }
-
-    /**
-     * Tests the labelPrimaryFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testLabelPrimaryFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label label-primary">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelPrimaryFunction($arg0));
-    }
-
-    /**
-     * Tests the labelSuccessFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testLabelSuccessFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label label-success">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelSuccessFunction($arg0));
-    }
-
-    /**
-     * Tests the labelSuccessFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testLabelWarningFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label label-warning">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->labelWarningFunction($arg0));
-    }
-
-    /**
-     * Tests the materialDesignLabelFunction() method.
-     *
-     * @return void
-     * @depends testGetFunctions
-     */
-    public function testMaterialDesignLabelFunction() {
-
-        $obj = new LabelUITwigExtension();
-
-        $arg0 = [];
-        $res0 = '<span class="label bg-red">&nbsp;</span>';
-        $this->assertEquals($res0, $obj->materialDesignLabelFunction($arg0));
+        $res0 = '<span class="label bg-red"></span>';
+        $this->assertEquals($res0, $obj->adminBSBMaterialDesignLabelFunction($arg0));
 
         $arg1 = ["content" => "content"];
         $res1 = '<span class="label bg-red">content</span>';
-        $this->assertEquals($res1, $obj->materialDesignLabelFunction($arg1));
+        $this->assertEquals($res1, $obj->adminBSBMaterialDesignLabelFunction($arg1));
 
         $arg7 = ["color" => "black"];
-        $res7 = '<span class="label bg-black">&nbsp;</span>';
-        $this->assertEquals($res7, $obj->materialDesignLabelFunction($arg7));
+        $res7 = '<span class="label bg-black"></span>';
+        $this->assertEquals($res7, $obj->adminBSBMaterialDesignLabelFunction($arg7));
 
         $arg9 = ["content" => "content", "color" => "black"];
         $res9 = '<span class="label bg-black">content</span>';
-        $this->assertEquals($res9, $obj->materialDesignLabelFunction($arg9));
+        $this->assertEquals($res9, $obj->adminBSBMaterialDesignLabelFunction($arg9));
     }
 
 }
--- Twig/Extension/UI/LabelUITwigExtension.php
+++ Twig/Extension/UI/LabelUITwigExtension.php
@@ -9,25 +9,34 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI;
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\UI;
 
 use Twig_SimpleFunction;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\AbstractABSBMDTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelComponentTwigExtension as BaseUITwigExtension;
 use WBW\Library\Core\Utility\Argument\ArrayUtility;
 
 /**
  * Label UI Twig extension.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\UI
  */
-class LabelUITwigExtension extends AbstractUITwigExtension {
+class LabelUITwigExtension extends BaseUITwigExtension {
 
     /**
      * Service name.
      *
      * @var string
      */
-    const SERVICE_NAME = "webeweb.bundle.adminbsbmaterialdesignbundle.twig.extension.ui.label";
+    const SERVICE_NAME = "webeweb.bundle.adminbsbbundle.twig.extension.ui.label";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
 
     /**
      * Get the Twig functions.
@@ -36,84 +45,24 @@ class LabelUITwigExtension extends AbstractUITwigExtension {
      */
     public function getFunctions() {
         return [
-            new Twig_SimpleFunction("labelDanger", [$this, "labelDangerFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("labelDefault", [$this, "labelDefaultFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("labelInfo", [$this, "labelInfoFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("labelPrimary", [$this, "labelPrimaryFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("labelSuccess", [$this, "labelSuccessFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("labelWarning", [$this, "labelWarningFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("materialDesignLabel", [$this, "materialDesignLabelFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelDanger", [$this, "bootstrapLabelDangerFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelDefault", [$this, "bootstrapLabelDefaultFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelInfo", [$this, "bootstrapLabelInfoFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelPrimary", [$this, "bootstrapLabelPrimaryFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelSuccess", [$this, "bootstrapLabelSuccessFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLabelWarning", [$this, "bootstrapLabelWarningFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBMaterialDesignLabel", [$this, "adminBSBMaterialDesignLabelFunction"], ["is_safe" => ["html"]]),
         ];
     }
 
     /**
-     * Displays a label "Danger".
+     * Displays an AdminBSB material design label.
      *
      * @param array $args The arguments.
-     * @return string Returns the label "Danger".
+     * @return string Returns the AdminBSB material design label.
      */
-    public function labelDangerFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-danger");
-    }
-
-    /**
-     * Displays a label "Default".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the label "Default".
-     */
-    public function labelDefaultFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-default");
-    }
-
-    /**
-     * Displays a label "Info".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the label "Info".
-     */
-    public function labelInfoFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-info");
-    }
-
-    /**
-     * Displays a label "Primary".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the label "Primary".
-     */
-    public function labelPrimaryFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-primary");
-    }
-
-    /**
-     * Displays a label "Success".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the label "Success".
-     */
-    public function labelSuccessFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-success");
-    }
-
-    /**
-     * Displays a label "Warning".
-     *
-     * @param array $args The arguments.
-     * @return string Returns the label "Warning".
-     */
-    public function labelWarningFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), "label-warning");
-    }
-
-    /**
-     * Displays a material design label.
-     *
-     * @param array $args The arguments.
-     * @return string Returns the material design label.
-     */
-    public function materialDesignLabelFunction(array $args = []) {
-        return $this->label(ArrayUtility::get($args, "content"), $this->getColor(ArrayUtility::get($args, "color", "red"), "bg-"));
+    public function adminBSBMaterialDesignLabelFunction(array $args = []) {
+        return $this->bootstrapLabel(ArrayUtility::get($args, "content"), AbstractABSBMDTwigExtension::fixColor(ArrayUtility::get($args, "color", "red"), "bg-"));
     }
 
 }
