9f6b1ed3c7da65d083a027e3ec866c760e9f4d68
webeweb <support@webeweb.fr>
2018-04-27 13:39:11 +0200

    Alias Bootstrap strong typgraphy Twig extension

---
+++ Twig/Extension/Typography/StrongTypographyTwigExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\Typography;
+
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Typography\StrongTypographyTwigExtension as BaseTypographyTwigExtension;
+
+/**
+ * Strong typography Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\Typography
+ */
+class StrongTypographyTwigExtension extends BaseTypographyTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.bootstrapbundle.twig.extension.typography.strong";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("absbmdStrong", [$this, "bootstrapStrongFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
