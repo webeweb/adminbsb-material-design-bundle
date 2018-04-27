a3b186c71815dd5d540f66c6f0fe7a68aa8c59ba
webeweb <support@webeweb.fr>
2018-04-27 13:38:28 +0200

    Alias Bootstrap inserted typgraphy Twig extension

---
+++ Tests/Twig/Extension/Typography/InsertedTypographyTwigExtensionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\Typography;
+
+use PHPUnit_Framework_TestCase;
+use Twig_Node;
+use Twig_SimpleFunction;
+use WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\Typography\InsertedTypographyTwigExtension;
+
+/**
+ * Inserted typography Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\Typography
+ * @final
+ */
+final class InsertedTypographyTwigExtensionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new InsertedTypographyTwigExtension();
+
+        $res = $obj->getFunctions();
+
+        $this->assertCount(1, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+        $this->assertEquals("absbmdInserted", $res[0]->getName());
+        $this->assertEquals([$obj, "bootstrapInsertedFunction"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+    }
+
+}
