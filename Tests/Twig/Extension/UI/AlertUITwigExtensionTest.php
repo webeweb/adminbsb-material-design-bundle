3f8b9036cfc7fd88ad24dd499738f6925bd494c7
webeweb <support@webeweb.fr>
2018-04-27 15:21:26 +0200

    Change Twig functions prefix

--- Tests/Twig/Extension/UI/AlertUITwigExtensionTest.php
+++ Tests/Twig/Extension/UI/AlertUITwigExtensionTest.php
@@ -9,18 +9,18 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\UI;
+namespace WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI;
 
 use PHPUnit_Framework_TestCase;
 use Twig_Node;
 use Twig_SimpleFunction;
-use WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI\AlertUITwigExtension;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\AlertUITwigExtension;
 
 /**
  * Alert UI Twig extension test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Tests\Twig\Extension\UI
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI
  * @final
  */
 final class AlertUITwigExtensionTest extends PHPUnit_Framework_TestCase {
@@ -39,69 +39,69 @@ final class AlertUITwigExtensionTest extends PHPUnit_Framework_TestCase {
         $this->assertCount(6, $res);
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
-        $this->assertEquals("absbmdBasicAlertDanger", $res[0]->getName());
+        $this->assertEquals("adminBSBBasicAlertDanger", $res[0]->getName());
         $this->assertEquals([$obj, "bootstrapBasicAlertDangerFunction"], $res[0]->getCallable());
         $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
-        $this->assertEquals("absbmdBasicAlertInfo", $res[1]->getName());
+        $this->assertEquals("adminBSBBasicAlertInfo", $res[1]->getName());
         $this->assertEquals([$obj, "bootstrapBasicAlertInfoFunction"], $res[1]->getCallable());
         $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
-        $this->assertEquals("absbmdBasicAlertSuccess", $res[2]->getName());
+        $this->assertEquals("adminBSBBasicAlertSuccess", $res[2]->getName());
         $this->assertEquals([$obj, "bootstrapBasicAlertSuccessFunction"], $res[2]->getCallable());
         $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
-        $this->assertEquals("absbmdBasicAlertWarning", $res[3]->getName());
+        $this->assertEquals("adminBSBBasicAlertWarning", $res[3]->getName());
         $this->assertEquals([$obj, "bootstrapBasicAlertWarningFunction"], $res[3]->getCallable());
         $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
-        $this->assertEquals("absbmdLinkAlert", $res[4]->getName());
+        $this->assertEquals("adminBSBLinkAlert", $res[4]->getName());
         $this->assertEquals([$obj, "bootstrapLinkAlertFunction"], $res[4]->getCallable());
         $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
 
         $this->assertInstanceOf(Twig_SimpleFunction::class, $res[5]);
-        $this->assertEquals("absbmdMaterialDesignAlert", $res[5]->getName());
-        $this->assertEquals([$obj, "absbmdMaterialDesignAlertFunction"], $res[5]->getCallable());
+        $this->assertEquals("adminBSBMaterialDesignAlert", $res[5]->getName());
+        $this->assertEquals([$obj, "adminBSBMaterialDesignAlertFunction"], $res[5]->getCallable());
         $this->assertEquals(["html"], $res[5]->getSafe(new Twig_Node()));
     }
 
     /**
-     * Tests the materialDesignAlertFunction() method.
+     * Tests the AdminBSBaterialDesignAlertFunction() method.
      *
      * @return void
      * @depends testGetFunctions
      */
-    public function testMaterialDesignAlertFunction() {
+    public function testAdminBSBMaterialDesignAlertFunction() {
 
         $obj = new AlertUITwigExtension();
 
         $arg0 = [];
         $res0 = '<div class="alert bg-red" role="alert"></div>';
-        $this->assertEquals($res0, $obj->absbmdMaterialDesignAlertFunction($arg0));
+        $this->assertEquals($res0, $obj->adminBSBMaterialDesignAlertFunction($arg0));
 
         $arg1 = ["content" => "content"];
         $res1 = '<div class="alert bg-red" role="alert">content</div>';
-        $this->assertEquals($res1, $obj->absbmdMaterialDesignAlertFunction($arg1));
+        $this->assertEquals($res1, $obj->adminBSBMaterialDesignAlertFunction($arg1));
 
         $arg2 = ["dismissible" => true];
         $res2 = '<div class="alert bg-red alert-dismissible" role="alert"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-        $this->assertEquals($res2, $obj->absbmdMaterialDesignAlertFunction($arg2));
+        $this->assertEquals($res2, $obj->adminBSBMaterialDesignAlertFunction($arg2));
 
         $arg3 = ["color" => "exception"];
         $res3 = '<div class="alert bg-red" role="alert"></div>';
-        $this->assertEquals($res3, $obj->absbmdMaterialDesignAlertFunction($arg3));
+        $this->assertEquals($res3, $obj->adminBSBMaterialDesignAlertFunction($arg3));
 
         $arg4 = ["color" => "black"];
         $res4 = '<div class="alert bg-black" role="alert"></div>';
-        $this->assertEquals($res4, $obj->absbmdMaterialDesignAlertFunction($arg4));
+        $this->assertEquals($res4, $obj->adminBSBMaterialDesignAlertFunction($arg4));
 
         $arg5 = ["content" => "content", "dismissible" => true, "color" => "black"];
         $res5 = '<div class="alert bg-black alert-dismissible" role="alert"><button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>content</div>';
-        $this->assertEquals($res5, $obj->absbmdMaterialDesignAlertFunction($arg5));
+        $this->assertEquals($res5, $obj->adminBSBMaterialDesignAlertFunction($arg5));
     }
 
 }
--- Twig/Extension/UI/AlertUITwigExtension.php
+++ Twig/Extension/UI/AlertUITwigExtension.php
@@ -9,10 +9,10 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI;
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\UI;
 
 use Twig_SimpleFunction;
-use WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\AbstractABSBMDTwigExtension;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\AbstractABSBMDTwigExtension;
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\AlertComponentTwigExtension as BaseUITwigExtension;
 use WBW\Library\Core\Utility\Argument\ArrayUtility;
 use WBW\Library\Core\Utility\Argument\StringUtility;
@@ -21,7 +21,7 @@ use WBW\Library\Core\Utility\Argument\StringUtility;
  * Alert UI Twig extension.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Bundle\AdminBSBMaterialDesignBundle\Twig\Extension\UI
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\UI
  */
 class AlertUITwigExtension extends BaseUITwigExtension {
 
@@ -30,7 +30,7 @@ class AlertUITwigExtension extends BaseUITwigExtension {
      *
      * @var string
      */
-    const SERVICE_NAME = "webeweb.bundle.adminbsbmaterialdesignbundle.twig.extension.ui.alert";
+    const SERVICE_NAME = "webeweb.bundle.adminbsbbundle.twig.extension.ui.alert";
 
     /**
      * Get the Twig functions.
@@ -39,12 +39,12 @@ class AlertUITwigExtension extends BaseUITwigExtension {
      */
     public function getFunctions() {
         return [
-            new Twig_SimpleFunction("absbmdBasicAlertDanger", [$this, "bootstrapBasicAlertDangerFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("absbmdBasicAlertInfo", [$this, "bootstrapBasicAlertInfoFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("absbmdBasicAlertSuccess", [$this, "bootstrapBasicAlertSuccessFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("absbmdBasicAlertWarning", [$this, "bootstrapBasicAlertWarningFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("absbmdLinkAlert", [$this, "bootstrapLinkAlertFunction"], ["is_safe" => ["html"]]),
-            new Twig_SimpleFunction("absbmdMaterialDesignAlert", [$this, "absbmdMaterialDesignAlertFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBBasicAlertDanger", [$this, "bootstrapBasicAlertDangerFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBBasicAlertInfo", [$this, "bootstrapBasicAlertInfoFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBBasicAlertSuccess", [$this, "bootstrapBasicAlertSuccessFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBBasicAlertWarning", [$this, "bootstrapBasicAlertWarningFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBLinkAlert", [$this, "bootstrapLinkAlertFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBMaterialDesignAlert", [$this, "adminBSBMaterialDesignAlertFunction"], ["is_safe" => ["html"]]),
         ];
     }
 
@@ -54,7 +54,7 @@ class AlertUITwigExtension extends BaseUITwigExtension {
      * @param array $args The arguments.
      * @return string Returns the AdminBSB material design alert.
      */
-    public function absbmdMaterialDesignAlertFunction(array $args = []) {
+    public function adminBSBMaterialDesignAlertFunction(array $args = []) {
         $template = $this->bootstrapAlertDangerFunction($args);
         $searches = ["alert-danger"];
         $replaces = [AbstractABSBMDTwigExtension::fixColor(ArrayUtility::get($args, "color", "red"), "bg-")];
