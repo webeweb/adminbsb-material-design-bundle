fd35e262b4bf58d3cc91b56a93ca8d8b446c8e92
webeweb <support@webeweb.fr>
2018-04-28 13:24:32 +0200

    Alias Bootstrap inline Twig extension

---
+++ Twig/Extension/Code/InlineCodeTwigExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\Code;
+
+use Twig_SimpleFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Code\InlineCodeTwigExtension as BaseTwigExtension;
+
+/**
+ * Inline code Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\Code
+ */
+class InlineCodeTwigExtension extends BaseTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.bundle.adminbsbbundle.twig.extension.code.inline";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("adminBSBInline", [$this, "bootstrapInlineFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
