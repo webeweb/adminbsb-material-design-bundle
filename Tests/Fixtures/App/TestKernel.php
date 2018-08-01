b371c7873de8b1d9f7a41b160c3e57171e5c666c
webeweb <support@webeweb.fr>
2018-08-01 09:48:16 +0200

    Add a TestKernel for further functionnal tests

---
+++ Tests/Fixtures/App/TestKernel.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapKernel;
+
+/**
+ * Test kernel.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\App
+ * @final
+ */
+final class TestKernel extends AbstractBootstrapKernel {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles() {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new WBW\Bundle\AdminBSBBundle\BootstrapBundle(),
+            new WBW\Bundle\BootstrapBundle\BootstrapBundle(),
+            new WBW\Bundle\SyntaxHighlighterBundle\SyntaxHighlighterBundle(),
+        ];
+        return $bundles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTestsDir() {
+        return __DIR__ . "/../..";
+    }
+
+}
