8fa1a092dc3345ddf8dba35c389b12d0c676220b
webeweb <support@webeweb.fr>
2018-08-07 17:19:26 +0200

    Asbtract AdminBSB framework test case inheritance

--- Tests/Twig/Extension/UI/BadgeTwigExtensionTest.php
+++ Tests/Twig/Extension/UI/BadgeTwigExtensionTest.php
@@ -11,9 +11,9 @@
 
 namespace WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI;
 
-use PHPUnit_Framework_TestCase;
 use Twig_Node;
 use Twig_SimpleFunction;
+use WBW\Bundle\AdminBSBBundle\Tests\Cases\AbstractAdminBSBFrameworkTestCase;
 use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\BadgeTwigExtension;
 
 /**
@@ -23,7 +23,7 @@ use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\BadgeTwigExtension;
  * @package WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI
  * @final
  */
-final class BadgeTwigExtensionTest extends PHPUnit_Framework_TestCase {
+final class BadgeTwigExtensionTest extends AbstractAdminBSBFrameworkTestCase {
 
     /**
      * Tests the getFunctions() method.
