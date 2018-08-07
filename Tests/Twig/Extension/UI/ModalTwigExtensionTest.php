c8b6ea5ba70332b4a6b6183d00e03c08b8950e5a
webeweb <support@webeweb.fr>
2018-08-07 17:19:48 +0200

    Asbtract AdminBSB framework test case inheritance

--- Tests/Twig/Extension/UI/ModalTwigExtensionTest.php
+++ Tests/Twig/Extension/UI/ModalTwigExtensionTest.php
@@ -11,9 +11,9 @@
 
 namespace WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI;
 
-use PHPUnit_Framework_TestCase;
 use Twig_Node;
 use Twig_SimpleFunction;
+use WBW\Bundle\AdminBSBBundle\Tests\Cases\AbstractAdminBSBFrameworkTestCase;
 use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\ModalTwigExtension;
 
 /**
@@ -23,7 +23,7 @@ use WBW\Bundle\AdminBSBBundle\Twig\Extension\UI\ModalTwigExtension;
  * @package WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\UI
  * @final
  */
-final class ModalTwigExtensionTest extends PHPUnit_Framework_TestCase {
+final class ModalTwigExtensionTest extends AbstractAdminBSBFrameworkTestCase {
 
     /**
      * Tests the getFunctions() method.
