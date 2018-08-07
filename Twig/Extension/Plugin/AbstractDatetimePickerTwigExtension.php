9338ab4f5fac2b2c30fcf454433323880c1cbeeb
webeweb <support@webeweb.fr>
2018-08-07 17:21:01 +0200

    Add Datetime picker Twig extension

---
+++ Twig/Extension/Plugin/AbstractDatetimePickerTwigExtension.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin;
+
+use Symfony\Component\Translation\TranslatorInterface;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\AbstractAdminBSBTwigExtension;
+use WBW\Library\Core\Helper\Argument\StringHelper;
+
+/**
+ * Abstract Datetime picker Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin
+ * @abstract
+ */
+abstract class AbstractDatetimePickerTwigExtension extends AbstractAdminBSBTwigExtension {
+
+    /**
+     * Datetime picker.
+     *
+     * @var string
+     */
+    const DATETIMEPICKER = <<< 'EOTXT'
+<script type="text/javascript">
+	$("%selector%").bootstrapMaterialDatePicker({
+		cancelText: "%cancelText%",
+		clearButton: %clearButton%,
+		clearText: "%clearText%",
+		date: %date%,
+		format: "%format%",
+		lang: "%lang%",
+		time: %time%,
+		weekStart: %weekStart%
+	});
+</script>
+EOTXT;
+
+    /**
+     * Default date format.
+     *
+     * @var string
+     */
+    const DEFAULT_DATE_FORMAT = "DD/MM/YYYY";
+
+    /**
+     * Default datetime format.
+     *
+     * @var string
+     */
+    const DEFAULT_DATETIME_FORMAT = self::DEFAULT_DATE_FORMAT . " - " . self::DEFAULT_TIME_FORMAT;
+
+    /**
+     * Default time format.
+     *
+     * @var string
+     */
+    const DEFAULT_TIME_FORMAT = "HH:mm";
+
+    /**
+     * Translator.
+     *
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * Constructor.
+     */
+    protected function __construct(TranslatorInterface $translator) {
+        $this->setTranslator($translator);
+    }
+
+    /**
+     * Display an AdminBSB datetime picker.
+     *
+     * @param string $selector The selector.
+     * @param string $clearButton Clear button ?
+     * @param boolean $date Date ?
+     * @param string $format The format.
+     * @param string $lang The lang.
+     * @param boolean $time Time ?
+     * @param string $weekStart The week start.
+     * @return string Returns the AdminBSB datetime picker.
+     */
+    protected function adminBSBDatetimePicker($selector, $clearButton, $date, $format, $lang, $time, $weekStart) {
+
+        // Initialize the values.
+        $weekStarts = [0, 1, 2, 3, 4, 5, 6];
+
+        // Initialize the parameters.
+        $cancelText   = $this->getTranslator()->trans("label.cancel", [], "BootstrapBundle");
+        $clearText    = $this->getTranslator()->trans("label.delete", [], "BootstrapBundle");
+        $bClearButton = StringHelper::parseBoolean($clearButton);
+        $bDate        = StringHelper::parseBoolean($date);
+        $bTime        = StringHelper::parseBoolean($time);
+        $iWeekStart   = true === in_array($weekStart, $weekStarts) ? $weekStart : 1;
+
+        //
+        $searches = ["%selector%", "%cancelText%", "%clearButton%", "%clearText%", "%date%", "%format%", "%lang%", "%time%", "%weekStart%"];
+        $replaces = [$selector, $cancelText, $bClearButton, $clearText, $bDate, $format, $lang, $bTime, $iWeekStart];
+
+        // Return the HTML.
+        return StringHelper::replace(self::DATETIMEPICKER, $searches, $replaces);
+    }
+
+    /**
+     * Get the translator.
+     *
+     * @return TranslatorInterface Returns the translator.
+     */
+    public function getTranslator() {
+        return $this->translator;
+    }
+
+    /**
+     * Set the translator.
+     *
+     * @param TranslatorInterface $translator The translator.
+     * @return AbstractDatetimePickerTwigExtension Returns this Datetime picker Twig extension.
+     */
+    protected function setTranslator(TranslatorInterface $translator) {
+        $this->translator = $translator;
+        return $this;
+    }
+
+}
---
+++ Twig/Extension/Plugin/DatetimePickerTwigExtension.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin;
+
+use Symfony\Component\Translation\TranslatorInterface;
+use Twig_SimpleFunction;
+use WBW\Library\Core\Helper\Argument\ArrayHelper;
+
+/**
+ * Datetime picker Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin
+ */
+class DatetimePickerTwigExtension extends AbstractDatetimePickerTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "webeweb.adminbsbbundle.twig.extension.plugin.datetimepicker";
+
+    /**
+     * Constructor.
+     *
+     * @param TranslatorInterface $translator The translator.
+     */
+    public function __construct(TranslatorInterface $translator) {
+        parent::__construct($translator);
+    }
+
+    /**
+     * Displays an AdminBSB date picker.
+     *
+     * @param $args The arguments.
+     * @return string Returns the AdminBSB date picker.
+     */
+    public function datePickerFunction(array $args = []) {
+        return $this->adminBSBDatetimePicker(ArrayHelper::get($args, "selector"), ArrayHelper::get($args, "clearButton", false), true, ArrayHelper::get($args, "format", self::DEFAULT_DATE_FORMAT), ArrayHelper::get($args, "lang", "en"), false, ArrayHelper::get($args, "weekStart", 1));
+    }
+
+    /**
+     * Displays a AdminBSB datetime picker.
+     *
+     * @param $args The arguments.
+     * @return string Returns the AdminBSB datetime picker.
+     */
+    public function datetimePickerFunction(array $args = []) {
+        return $this->adminBSBDatetimePicker(ArrayHelper::get($args, "selector"), ArrayHelper::get($args, "clearButton", false), ArrayHelper::get($args, "date", false), ArrayHelper::get($args, "format", self::DEFAULT_DATE_FORMAT), ArrayHelper::get($args, "lang", "en"), ArrayHelper::get($args, "time", false), ArrayHelper::get($args, "weekStart", 1));
+    }
+
+    /**
+     * Displays a time picker.
+     *
+     * @param $args The arguments.
+     * @return string Returns the AdminBSB time picker.
+     */
+    public function timePickerFunction(array $args = []) {
+        return $this->adminBSBDatetimePicker(ArrayHelper::get($args, "selector"), ArrayHelper::get($args, "clearButton", false), false, ArrayHelper::get($args, "format", self::DEFAULT_TIME_FORMAT), ArrayHelper::get($args, "lang", "en"), true, ArrayHelper::get($args, "weekStart", 1));
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return array Returns the Twig functions.
+     */
+    public function getFunctions() {
+        return [
+            new Twig_SimpleFunction("adminBSBDatePicker", [$this, "adminBSBDatePickerFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBDatetimePicker", [$this, "adminBSBDatetimePickerFunction"], ["is_safe" => ["html"]]),
+            new Twig_SimpleFunction("adminBSBTimePicker", [$this, "adminBSBTimePickerFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+}
