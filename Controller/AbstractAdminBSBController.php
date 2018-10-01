fbd81e6861082333f26ef18c28604c8c958b8de5
webeweb <support@webeweb.fr>
2018-10-01 13:57:02 +0200

    Add an abstract Admin BSB controller

---
+++ Controller/AbstractAdminBSBController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Controller;
+
+use WBW\Bundle\BootstrapBundle\Controller\AbstractBootstrapController;
+
+/**
+ * Abstract AdminBSB controller.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Controller
+ * @abstract
+ */
+abstract class AbstractAdminBSBController extends AbstractBootstrapController {
+
+}
