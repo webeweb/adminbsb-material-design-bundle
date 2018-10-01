74535723e884eb39d31b326a72b70cee3dd4554e
webeweb <support@webeweb.fr>
2018-10-01 15:23:01 +0200

    Add a Layout controller test

---
+++ Tests/Controller/LayoutControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Controller;
+
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractWebTestCase;
+
+/**
+ * Layout controller test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Controller
+ * @final
+ */
+final class LayoutControllerTest extends AbstractWebTestCase {
+
+    /**
+     * Tests the blankAction() method.
+     *
+     * @return void
+     */
+    public function testBlankAction() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request.
+        $client->request("GET", "/blank");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
