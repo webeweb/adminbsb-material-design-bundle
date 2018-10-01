ea108cb7c614c2ae4bbc2b058f596d13d072c625
webeweb <support@webeweb.fr>
2018-10-01 14:03:34 +0200

    Add a test layout controller

---
+++ Tests/Fixtures/Controller/TestLayoutController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use WBW\Bundle\AdminBSBBundle\Controller\AbstractAdminBSBController;
+
+/**
+ * Test layout controller.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Controller
+ * @final
+ */
+final class TestLayoutController extends AbstractAdminBSBController {
+
+    /**
+     * Displays a blank page.
+     *
+     * @return Response Returns the response.
+     */
+    public function blankAction() {
+
+        // Return the response.
+        return $this->render("@AdminBSB/layout/blank.html.twig");
+    }
+
+}
