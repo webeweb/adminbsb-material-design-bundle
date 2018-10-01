ac84413596674470041b690a39370cb0b9faad06
webeweb <support@webeweb.fr>
2018-10-01 15:17:30 +0200

    Add a user mock (for FOSUser)

---
+++ Tests/Fixtures/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User entity.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Entity
+ * @final
+ */
+class User extends BaseUser {
+
+}
