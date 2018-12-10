56701ed16094babd583b268776521cfbc5ef344e
webeweb <support@webeweb.fr>
2018-12-10 17:50:28 +0100

    Add default user info theme provider

---
+++ Theme/DefaultUserInfoThemeProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Theme\DefaultUserInfoThemeProvider as BaseUserInfoThemeProvider;
+
+/**
+ * Default user info theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Theme
+ */
+class DefaultUserInfoThemeProvider extends BaseUserInfoThemeProvider {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
