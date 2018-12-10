885cbf4007ff5d37809c79e0ea5db38fb744bc2a
webeweb <support@webeweb.fr>
2018-12-10 17:48:26 +0100

    Add default application theme provider

---
+++ Tests/Theme/DefaultApplicationThemeProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Theme;
+
+use WBW\Bundle\AdminBSBBundle\Theme\DefaultApplicationThemeProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Default application theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Theme
+ */
+class DefaultApplicationThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultApplicationThemeProvider();
+
+        $this->assertEquals("AdminBSB bundle", $obj->getDescription());
+        $this->assertEquals("AdminBSB<b>bundle</b>", $obj->getName());
+        $this->assertEquals("AdminBSB bundle", $obj->getTitle());
+    }
+
+}
---
+++ Theme/DefaultApplicationThemeProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Theme\DefaultApplicationThemeProvider as BaseApplicationThemeProvider;
+
+/**
+ * Default application theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Theme
+ */
+class DefaultApplicationThemeProvider extends BaseApplicationThemeProvider {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription() {
+        return "AdminBSB bundle";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return "AdminBSB<b>bundle</b>";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle() {
+        return "AdminBSB bundle";
+    }
+
+}
