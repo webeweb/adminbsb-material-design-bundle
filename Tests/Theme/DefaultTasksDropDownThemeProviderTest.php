200f4144db204603e340383cbee7ad5d7319f56d
webeweb <support@webeweb.fr>
2018-12-10 17:50:18 +0100

    Add default tasks drop down theme provider

---
+++ Tests/Theme/DefaultTasksDropDownThemeProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Theme;
+
+use WBW\Bundle\AdminBSBBundle\Theme\DefaultTasksDropDownThemeProvider;
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase;
+
+/**
+ * Default tasks drop down theme provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Theme
+ */
+class DefaultTasksDropDownThemeProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DefaultTasksDropDownThemeProvider();
+
+        $this->assertNotNull($obj);
+    }
+
+}
---
+++ Theme/DefaultTasksDropDownThemeProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Theme\DefaultTasksDropDownThemeProvider as BaseTasksDropDownThemeProvider;
+
+/**
+ * Default tasks drop down theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Theme
+ */
+class DefaultTasksDropDownThemeProvider extends BaseTasksDropDownThemeProvider {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
