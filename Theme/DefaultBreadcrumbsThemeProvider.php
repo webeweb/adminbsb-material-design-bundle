927ac5de723567ae508ace1d7bd94cbf6d8a1038
webeweb <support@webeweb.fr>
2018-12-10 17:49:18 +0100

    Add default breadcrumbs theme provider

---
+++ Theme/DefaultBreadcrumbsThemeProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Theme;
+
+use WBW\Bundle\CoreBundle\Theme\DefaultBreadcrumbsThemeProvider as BaseBreadcrumbsThemeProvider;
+
+/**
+ * Default breadcrumbs theme provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Theme
+ */
+class DefaultBreadcrumbsThemeProvider extends BaseBreadcrumbsThemeProvider {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
