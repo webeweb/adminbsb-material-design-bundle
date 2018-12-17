00dd5b6d37c69cd3e75fca0a9f0d15f137922bdf
webeweb <support@webeweb.fr>
2018-12-17 17:07:14 +0100

    Update file headers

--- Tests/Twig/Extension/AdminBSBRendererTwigExtensionTest.php
+++ Tests/Twig/Extension/AdminBSBRendererTwigExtensionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * This file is part of the adminbsb-material-design-bundle ackage.
  *
  * (c) 2018 WEBEWEB
--- Theme/DefaultApplicationThemeProvider.php
+++ Theme/DefaultApplicationThemeProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * This file is part of the adminbsb-material-design-bundle package.
  *
  * (c) 2018 WEBEWEB
