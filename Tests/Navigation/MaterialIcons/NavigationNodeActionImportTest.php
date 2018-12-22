1d12fb9a296ee391b942b7962af04b62d944f429
webeweb <support@webeweb.fr>
2018-12-22 11:35:46 +0100

    Add navigation node action "Import"

---
+++ Tests/Navigation/MaterialIcons/NavigationNodeActionImportTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons;
+
+use WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons\NavigationNodeActionImport;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+
+/**
+ * Navigation node action "Import" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons
+ */
+class NavigationNodeActionImportTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeActionImport("route");
+
+        $this->assertEquals("navigation.node.action.import", $obj->getId());
+        $this->assertEquals("mi:file_upload", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
