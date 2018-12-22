2e658138e0bd818d8e16f57f77b522a993ccb6b6
webeweb <support@webeweb.fr>
2018-12-22 11:25:07 +0100

    Add breadcrumb node action "Edit"

---
+++ Tests/Navigation/MaterialIcons/BreadcrumbNodeActionEditTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons;
+
+use WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons\BreadcrumbNodeActionEdit;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+
+/**
+ * Breadcrumb node action "Edit" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons
+ */
+class BreadcrumbNodeActionEditTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BreadcrumbNodeActionEdit("route");
+
+        $this->assertEquals("navigation.node.action.edit", $obj->getId());
+        $this->assertEquals("mi:pencil", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
