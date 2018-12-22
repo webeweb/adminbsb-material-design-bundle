5427362d4342080540d8d897021e5a305d8d7fe8
webeweb <support@webeweb.fr>
2018-12-22 11:35:03 +0100

    Add breadcrumb node action "New user"

---
+++ Navigation/MaterialIcons/BreadcrumbNodeActionNewUser.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons;
+
+use WBW\Bundle\CoreBundle\Navigation\BreadcrumbNode;
+
+/**
+ * Breadcrumb node action "New user".
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons
+ */
+class BreadcrumbNodeActionNewUser extends BreadcrumbNode {
+
+    /**
+     * Constructor.
+     *
+     * @param string $route The route.
+     * @param string $matcher The matcher.
+     */
+    public function __construct($route = null, $matcher = self::NAVIGATION_MATCHER_URL) {
+        parent::__construct("navigation.node.action.new", "mi:person_add", $route, $matcher);
+    }
+
+}
---
+++ Tests/Navigation/MaterialIcons/BreadcrumbNodeActionNewUserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons;
+
+use WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons\BreadcrumbNodeActionNewUser;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+
+/**
+ * Breadcrumb node action "New user" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons
+ */
+class BreadcrumbNodeActionNewUserTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BreadcrumbNodeActionNewUser("route");
+
+        $this->assertEquals("navigation.node.action.new", $obj->getId());
+        $this->assertEquals("mi:person_add", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
