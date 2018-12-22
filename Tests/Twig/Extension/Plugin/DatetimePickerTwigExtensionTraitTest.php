e6f93f0370347c2e81cd8e4a30562b0d3deb6874
webeweb <support@webeweb.fr>
2018-12-22 13:51:16 +0100

    Add Datetime picker Twig extension trait

---
+++ Tests/Twig/Extension/Plugin/DatetimePickerTwigExtensionTraitTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\Plugin;
+
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Twig\Extension\Plugin\TestDatetimePickerTwigExtensionTrait;
+use WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin\DatetimePickerTwigExtension;
+
+/**
+ * Datetime picker Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Twig\Extension\Plugin
+ */
+class DatetimePickerTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestDatetimePickerTwigExtensionTrait();
+
+        $this->assertNull($obj->getDatetimePickerTwigExtension());
+    }
+
+    /**
+     * Tests the setDatetimePickerTwigExtension() method.
+     *
+     * @return void
+     */
+    public function testSetDatetimePickerTwigExtension() {
+
+        // Set a Datetime picket Twig extension mock.
+        $datetimePickerTwigExtension = new DatetimePickerTwigExtension($this->twigEnvironment, $this->translator);
+
+        $obj = new TestDatetimePickerTwigExtensionTrait();
+
+        $obj->setDatetimePickerTwigExtension($datetimePickerTwigExtension);
+        $this->assertSame($datetimePickerTwigExtension, $obj->getDatetimePickerTwigExtension());
+    }
+
+}
---
+++ Twig/Extension/Plugin/DatetimePickerTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin;
+
+/**
+ * Datetime picker Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\Plugin
+ */
+trait DatetimePickerTwigExtensionTrait {
+
+    /**
+     * Datetime picker Twig extension.
+     *
+     * @var DatetimePickerTwigExtension
+     */
+    private $datetimePickerTwigExtension;
+
+    /**
+     * Get the datetime picker Twig extension.
+     *
+     * @return DatetimePickerTwigExtension Returns the datetime picker Twig extension.
+     */
+    public function getDatetimePickerTwigExtension() {
+        return $this->datetimePickerTwigExtension;
+    }
+
+    /**
+     * Set the datetime picker Twig extension.
+     *
+     * @param DatetimePickerTwigExtension $datetimePickerTwigExtension The datetime picker Twig extension.
+     */
+    protected function setDatetimePickerTwigExtension(DatetimePickerTwigExtension $datetimePickerTwigExtension) {
+        $this->datetimePickerTwigExtension = $datetimePickerTwigExtension;
+        return $this;
+    }
+
+}
