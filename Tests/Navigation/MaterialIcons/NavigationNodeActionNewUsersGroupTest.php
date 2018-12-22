affe6a342ac7439f870167e9ad525435289432cc
webeweb <support@webeweb.fr>
2018-12-22 11:36:31 +0100

    Add navigation node action "New users group"

---
+++ Tests/Navigation/MaterialIcons/NavigationNodeActionNewUsersGroupTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons;
+
+use WBW\Bundle\AdminBSBBundle\Navigation\MaterialIcons\NavigationNodeActionNewUsersGroup;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+
+/**
+ * Navigation node action "New users group" test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Navigation\MaterialIcons
+ */
+class NavigationNodeActionNewUsersGroupTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NavigationNodeActionNewUsersGroup("route");
+
+        $this->assertEquals("navigation.node.action.new", $obj->getId());
+        $this->assertEquals("mi:group_add", $obj->getIcon());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+
+}
