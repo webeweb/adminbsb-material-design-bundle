c841166ae7c26a9115402c8706d4cc4fccf036e2
webeweb <support@webeweb.fr>
2018-12-24 16:07:17 +0100

    Add Badge Twig extension trait

---
+++ Twig/Extension/UI/BadgeTwigExtensionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Twig\Extension\UI;
+
+/**
+ * Badge Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Twig\Extension\UI
+ */
+trait BadgeTwigExtensionTrait {
+
+    /**
+     * Badge Twig extension.
+     *
+     * @var BadgeTwigExtension
+     */
+    private $badgeTwigExtension;
+
+    /**
+     * Get the badge Twig extension.
+     *
+     * @return BadgeTwigExtension Returns the badge Twig extension.
+     */
+    public function getBadgeTwigExtension() {
+        return $this->badgeTwigExtension;
+    }
+
+    /**
+     * Set the badge Twig extension.
+     *
+     * @param BadgeTwigExtension $badgeTwigExtension The badge Twig extension.
+     */
+    protected function setBadgeTwigExtension($badgeTwigExtension) {
+        $this->badgeTwigExtension = $badgeTwigExtension;
+        return $this;
+    }
+
+
+
+}
