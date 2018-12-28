d46f85d684251fb3fc57188e81b35315dbc82abf
webeweb <support@webeweb.fr>
2018-12-28 14:21:48 +0100

    Reorder Typography Twig extension unit tests

--- Tests/Twig/Extension/Typography/TypographyTwigExtensionTest.php
+++ Tests/Twig/Extension/Typography/TypographyTwigExtensionTest.php
@@ -25,57 +25,6 @@ use WBW\Bundle\AdminBSBBundle\Twig\Extension\Typography\TypographyTwigExtension;
 class TypographyTwigExtensionTest extends AbstractTestCase {
 
     /**
-     * Tests the __construct() method.
-     *
-     * @return void
-     */
-    public function testConstruct() {
-
-        $obj = new TypographyTwigExtension($this->twigEnvironment);
-
-        $this->assertEquals("webeweb.adminbsb.twig.extension.typography", TypographyTwigExtension::SERVICE_NAME);
-        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
-    }
-
-    /**
-     * Tests the getFunctions() method.
-     *
-     * @return void
-     */
-    public function testGetFunctions() {
-
-        $obj = new TypographyTwigExtension($this->twigEnvironment);
-
-        $res = $obj->getFunctions();
-        $this->assertCount(5, $res);
-
-        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
-        $this->assertEquals("adminBSBBold", $res[0]->getName());
-        $this->assertEquals([$obj, "adminBSBBoldFunction"], $res[0]->getCallable());
-        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
-
-        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
-        $this->assertEquals("adminBSBItalic", $res[1]->getName());
-        $this->assertEquals([$obj, "adminBSBItalicFunction"], $res[1]->getCallable());
-        $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
-
-        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
-        $this->assertEquals("adminBSBOverline", $res[2]->getName());
-        $this->assertEquals([$obj, "adminBSBOverlineFunction"], $res[2]->getCallable());
-        $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
-
-        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
-        $this->assertEquals("adminBSBLineThrough", $res[3]->getName());
-        $this->assertEquals([$obj, "adminBSBLineThroughFunction"], $res[3]->getCallable());
-        $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
-
-        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
-        $this->assertEquals("adminBSBUnderline", $res[4]->getName());
-        $this->assertEquals([$obj, "adminBSBUnderlineFunction"], $res[4]->getCallable());
-        $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
-    }
-
-    /**
      * Tests the adminBSBBoldFunction() method.
      *
      * @return void
@@ -132,34 +81,6 @@ class TypographyTwigExtensionTest extends AbstractTestCase {
     }
 
     /**
-     * Tests the adminBSBOverlineFunction() method.
-     *
-     * @return void
-     */
-    public function testAdminBSBOverlineFunction() {
-
-        $obj = new TypographyTwigExtension($this->twigEnvironment);
-
-        $arg = ["content" => "content"];
-        $res = '<span class="font-overline">content</span>';
-        $this->assertEquals($res, $obj->adminBSBOverlineFunction($arg));
-    }
-
-    /**
-     * Tests the adminBSBOverlineFunction() method.
-     *
-     * @return void
-     */
-    public function testAdminBSBOverlineFunctionWithoutArguments() {
-
-        $obj = new TypographyTwigExtension($this->twigEnvironment);
-
-        $arg = [];
-        $res = '<span class="font-overline"></span>';
-        $this->assertEquals($res, $obj->adminBSBOverlineFunction($arg));
-    }
-
-    /**
      * Tests the adminBSBLineThroughFunction() method.
      *
      * @return void
@@ -188,6 +109,34 @@ class TypographyTwigExtensionTest extends AbstractTestCase {
     }
 
     /**
+     * Tests the adminBSBOverlineFunction() method.
+     *
+     * @return void
+     */
+    public function testAdminBSBOverlineFunction() {
+
+        $obj = new TypographyTwigExtension($this->twigEnvironment);
+
+        $arg = ["content" => "content"];
+        $res = '<span class="font-overline">content</span>';
+        $this->assertEquals($res, $obj->adminBSBOverlineFunction($arg));
+    }
+
+    /**
+     * Tests the adminBSBOverlineFunction() method.
+     *
+     * @return void
+     */
+    public function testAdminBSBOverlineFunctionWithoutArguments() {
+
+        $obj = new TypographyTwigExtension($this->twigEnvironment);
+
+        $arg = [];
+        $res = '<span class="font-overline"></span>';
+        $this->assertEquals($res, $obj->adminBSBOverlineFunction($arg));
+    }
+
+    /**
      * Tests the adminBSBUnderlineFunction() method.
      *
      * @return void
@@ -213,6 +162,57 @@ class TypographyTwigExtensionTest extends AbstractTestCase {
         $arg = [];
         $res = '<span class="font-underline"></span>';
         $this->assertEquals($res, $obj->adminBSBUnderlineFunction($arg));
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TypographyTwigExtension($this->twigEnvironment);
+
+        $this->assertEquals("webeweb.adminbsb.twig.extension.typography", TypographyTwigExtension::SERVICE_NAME);
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+
+    /**
+     * Tests the getFunctions() method.
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new TypographyTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(5, $res);
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[0]);
+        $this->assertEquals("adminBSBBold", $res[0]->getName());
+        $this->assertEquals([$obj, "adminBSBBoldFunction"], $res[0]->getCallable());
+        $this->assertEquals(["html"], $res[0]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[1]);
+        $this->assertEquals("adminBSBItalic", $res[1]->getName());
+        $this->assertEquals([$obj, "adminBSBItalicFunction"], $res[1]->getCallable());
+        $this->assertEquals(["html"], $res[1]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[2]);
+        $this->assertEquals("adminBSBOverline", $res[2]->getName());
+        $this->assertEquals([$obj, "adminBSBOverlineFunction"], $res[2]->getCallable());
+        $this->assertEquals(["html"], $res[2]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[3]);
+        $this->assertEquals("adminBSBLineThrough", $res[3]->getName());
+        $this->assertEquals([$obj, "adminBSBLineThroughFunction"], $res[3]->getCallable());
+        $this->assertEquals(["html"], $res[3]->getSafe(new Twig_Node()));
+
+        $this->assertInstanceOf(Twig_SimpleFunction::class, $res[4]);
+        $this->assertEquals("adminBSBUnderline", $res[4]->getName());
+        $this->assertEquals([$obj, "adminBSBUnderlineFunction"], $res[4]->getCallable());
+        $this->assertEquals(["html"], $res[4]->getSafe(new Twig_Node()));
     }
 
 }
