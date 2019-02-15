c267d1b0c209b3979f087d31838626d1f4577a12
webeweb <support@webeweb.fr>
2019-02-15 18:19:20 +0100

    Add Material Design button

---
+++ Button/MaterialDesignButton.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WBW\Bundle\AdminBSBBundle\Button;
+
+use WBW\Bundle\BootstrapBundle\Button\AbstractButton;
+
+/**
+ * Material Design button.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Button
+ */
+class MaterialDesignButton extends AbstractButton {
+
+    /**
+     * Constructor.
+     *
+     * @param string $color The color.
+     */
+    public function __construct($color) {
+        parent::__construct($color);
+    }
+}
---
+++ Tests/Button/MaterialDesignButtonTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Button;
+
+use WBW\Bundle\AdminBSBBundle\Button\MaterialDesignButton;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+
+/**
+ * Material Design button test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Button
+ */
+class MaterialDesignButtonTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new MaterialDesignButton("color");
+
+        $this->assertEquals("color", $obj->getType());
+    }
+}
