f6de09c371900f6e9bfaa03d5f90ade4a8c07ab8
webeweb <support@webeweb.fr>
2019-02-18 18:08:51 +0100

    Update Test layout controller

--- Tests/Fixtures/Controller/TestLayoutController.php
+++ Tests/Fixtures/Controller/TestLayoutController.php
@@ -11,8 +11,9 @@
 
 namespace WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Controller;
 
+use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Component\HttpFoundation\Response;
-use WBW\Bundle\AdminBSBBundle\Controller\AbstractAdminBSBController;
+use WBW\Bundle\AdminBSBBundle\Controller\AbstractController;
 
 /**
  * Test layout controller.
@@ -20,7 +21,7 @@ use WBW\Bundle\AdminBSBBundle\Controller\AbstractAdminBSBController;
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Controller
  */
-class TestLayoutController extends AbstractAdminBSBController {
+class TestLayoutController extends AbstractController {
 
     /**
      * Displays a blank page.
@@ -31,5 +32,63 @@ class TestLayoutController extends AbstractAdminBSBController {
 
         // Return the response.
         return $this->render("@AdminBSB/layout/blank.html.twig");
+    }
+
+    /**
+     * Displays an error template.
+     *
+     * @return Response Returns the response.
+     */
+    public function error404Action() {
+
+        // Return the response.
+        return $this->render("@AdminBSB/layout/exception/error404.html.twig", [
+            "status_code" => 404,
+            "status_text" => "Not found",
+        ]);
+    }
+
+    /**
+     * Displays an error template.
+     *
+     * @return Response Returns the response.
+     */
+    public function error500Action() {
+
+        // Return the response.
+        return $this->render("@AdminBSB/layout/exception/error500.html.twig", [
+            "status_code" => 500,
+            "status_text" => "Internal server error",
+        ]);
+    }
+
+    /**
+     * Displays a login template.
+     *
+     * @return Response Returns the response.
+     */
+    public function loginAction() {
+
+        // Return the response.
+        return $this->render("@AdminBSB/layout/security/login.html.twig", [
+            "last_username" => "",
+            "csrf_token"    => "",
+        ]);
+    }
+
+    /**
+     * Displays a register template.
+     *
+     * @return Response Returns the response.
+     */
+    public function registerAction() {
+
+        // Creates the form.
+        $form = $this->createForm(RegistrationFormType::class, new TestUser());
+
+        // Return the response.
+        return $this->render("@AdminBSB/layout/registration/register.html.twig", [
+            "form" => $form->createView(),
+        ]);
     }
 }
