667ffa4cf5b5b568cc7ef1fafa39671eaba0f2c6
webeweb <support@webeweb.fr>
2019-02-18 18:09:31 +0100

    Update Layout controller test

--- Tests/Controller/LayoutControllerTest.php
+++ Tests/Controller/LayoutControllerTest.php
@@ -35,4 +35,54 @@ class LayoutControllerTest extends AbstractWebTestCase {
         $client->request("GET", "/blank");
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    /**
+     * Tests the error404Action() method.
+     *
+     * @return void
+     */
+    public function testError404Action() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request.
+        $client->request("GET", "/error404");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        // Get the response.
+        $response = $client->getResponse()->getContent();
+
+        $this->assertContains("<title>AdminBSB Material Design - 404 Not found</title>", $response);
+
+        $this->assertContains("<body class=\"four-zero-four\">", $response);
+
+        $this->assertContains("<div class=\"error-code\">404</div>", $response);
+        $this->assertContains("<div class=\"error-message\">Not found</div>", $response);
+    }
+
+    /**
+     * Tests the error500Action() method.
+     *
+     * @return void
+     */
+    public function testError500Action() {
+
+        // Create a client.
+        $client = static::createClient();
+
+        // Make a GET request.
+        $client->request("GET", "/error500");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        // Get the response.
+        $response = $client->getResponse()->getContent();
+
+        $this->assertContains("<title>AdminBSB Material Design - 500 Internal server error</title>", $response);
+
+        $this->assertContains("<body class=\"five-zero-zero\">", $response);
+
+        $this->assertContains("<div class=\"error-code\">500</div>", $response);
+        $this->assertContains("<div class=\"error-message\">Internal server error</div>", $response);
+    }
 }
