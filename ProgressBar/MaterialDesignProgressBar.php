6ef764c7efb476f972c3a5a387d8f1f47b9fb184
webeweb <support@webeweb.fr>
2019-02-20 17:39:02 +0100

    Add Material Design progress bar

---
+++ ProgressBar/MaterialDesignProgressBar.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\ProgressBar;
+
+use WBW\Bundle\BootstrapBundle\ProgressBar\AbstractProgressBar;
+
+/**
+ * Material Design progress bar.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\ProgressBar
+ */
+class MaterialDesignProgressBar extends AbstractProgressBar {
+
+    /**
+     * Constructor.
+     *
+     * @param $color
+     */
+    public function __construct($color) {
+        parent::__construct($color);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrefix() {
+        return "bg-";
+    }
+}
---
+++ Tests/ProgressBar/MaterialDesignProgressBarTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\ProgressBar;
+
+use WBW\Bundle\AdminBSBBundle\ProgressBar\MaterialDesignProgressBar;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+
+/**
+ * Material Design progress bar test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\ProgressBar
+ */
+class MaterialDesignProgressBarTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new MaterialDesignProgressBar("color");
+
+        $this->assertEquals("bg-", $obj->getPrefix());
+        $this->assertEquals("color", $obj->getType());
+    }
+}
