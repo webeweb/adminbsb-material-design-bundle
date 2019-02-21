9eda89dd3818a0bf15472c485189385591a038ba
webeweb <support@webeweb.fr>
2019-02-21 17:19:00 +0100

    Move Twig error templates

--- Tests/Fixtures/Controller/TestLayoutController.php
+++ Tests/Fixtures/Controller/TestLayoutController.php
@@ -11,7 +11,6 @@
 
 namespace WBW\Bundle\AdminBSBBundle\Tests\Fixtures\Controller;
 
-use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Component\HttpFoundation\Response;
 use WBW\Bundle\AdminBSBBundle\Controller\AbstractController;
 
@@ -42,7 +41,7 @@ class TestLayoutController extends AbstractController {
     public function error404Action() {
 
         // Return the response.
-        return $this->render("@AdminBSB/layout/exception/error404.html.twig", [
+        return $this->render("@AdminBSB/layout/Exception/error404.html.twig", [
             "status_code" => 404,
             "status_text" => "Not found",
         ]);
@@ -56,39 +55,9 @@ class TestLayoutController extends AbstractController {
     public function error500Action() {
 
         // Return the response.
-        return $this->render("@AdminBSB/layout/exception/error500.html.twig", [
+        return $this->render("@AdminBSB/layout/Exception/error500.html.twig", [
             "status_code" => 500,
             "status_text" => "Internal server error",
-        ]);
-    }
-
-    /**
-     * Displays a login template.
-     *
-     * @return Response Returns the response.
-     */
-    public function loginAction() {
-
-        // Return the response.
-        return $this->render("@AdminBSB/layout/security/login.html.twig", [
-            "last_username" => "",
-            "csrf_token"    => "",
-        ]);
-    }
-
-    /**
-     * Displays a register template.
-     *
-     * @return Response Returns the response.
-     */
-    public function registerAction() {
-
-        // Creates the form.
-        $form = $this->createForm(RegistrationFormType::class, new TestUser());
-
-        // Return the response.
-        return $this->render("@AdminBSB/layout/registration/register.html.twig", [
-            "form" => $form->createView(),
         ]);
     }
 }
