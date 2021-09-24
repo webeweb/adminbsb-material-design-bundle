6f719931e6c225c6a91f79fa7ae8f948d9602b7b
webeweb <support@webeweb.fr>
2021-09-24 16:32:21 +0200

    Add Infobox 3

---
+++ Tests/Asset/Widget/Infobox3Test.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Asset\Widget;
+
+use WBW\Bundle\AdminBSBBundle\Asset\Widget\Infobox3;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+
+/**
+ * Infobox 3 test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Asset\Widget
+ */
+class Infobox3Test extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Infobox3();
+
+        $this->assertEquals("info-box-2", $obj->getClass());
+    }
+}
