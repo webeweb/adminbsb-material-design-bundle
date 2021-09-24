1688af4f66672e07993bae728a0cdad50f710ef1
webeweb <support@webeweb.fr>
2021-09-24 16:32:28 +0200

    Add Infobox 4

---
+++ Asset/Widget/Infobox4.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Asset\Widget;
+
+/**
+ * Infobox 4.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Asset\Widget
+ */
+class Infobox4 extends AbstractInfobox {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct("info-box-3");
+    }
+}
---
+++ Tests/Asset/Widget/Infobox4Test.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Asset\Widget;
+
+use WBW\Bundle\AdminBSBBundle\Asset\Widget\Infobox4;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+
+/**
+ * Infobox 4 test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Asset\Widget
+ */
+class Infobox4Test extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Infobox4();
+
+        $this->assertEquals("info-box-3", $obj->getClass());
+    }
+}
