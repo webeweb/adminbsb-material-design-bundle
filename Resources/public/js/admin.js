1f32e44249f79f02f8ba4b8c2b2acf76a1f2d340
webeweb <support@webeweb.fr>
2021-12-14 11:04:33 +0100

    Update javascript files

--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -96,11 +96,11 @@ $.AdminBSB.leftSideBar = {
         });
 
         //Set menu height
-        _this.setMenuHeight();
-        _this.checkStatuForResize(true);
+        _this.setMenuHeight(true);
+        _this.checkStatusForResize(true);
         $(window).resize(function () {
-            _this.setMenuHeight();
-            _this.checkStatuForResize(false);
+            _this.setMenuHeight(false);
+            _this.checkStatusForResize(false);
         });
 
         //Set Waves
@@ -113,6 +113,12 @@ $.AdminBSB.leftSideBar = {
             var height = ($(window).height() - ($('.legal').outerHeight() + $('.user-info').outerHeight() + $('.navbar').innerHeight()));
             var $el = $('.list');
 
+            if (!isFirstTime) {
+                $el.slimscroll({
+                    destroy: true
+                });
+            }
+
             $el.slimscroll({
                 height: height + "px",
                 color: configs.scrollColor,
@@ -124,12 +130,15 @@ $.AdminBSB.leftSideBar = {
 
             //Scroll active menu item when page load, if option set = true
             if ($.AdminBSB.options.leftSideBar.scrollActiveItemWhenPageLoad) {
-                var activeItemOffsetTop = $('.menu .list li.active')[0].offsetTop
-                if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                var item = $('.menu .list li.active')[0];
+                if (item) {
+                    var activeItemOffsetTop = item.offsetTop;
+                    if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                }
             }
         }
     },
-    checkStatuForResize: function (firstTime) {
+    checkStatusForResize: function (firstTime) {
         var $body = $('body');
         var $openCloseBar = $('.navbar .navbar-header .bars');
         var width = $body.width();
@@ -259,14 +268,16 @@ $.AdminBSB.navbar = {
 *  
 */
 $.AdminBSB.input = {
-    activate: function () {
+    activate: function ($parentSelector) {
+        $parentSelector = $parentSelector || $('body');
+
         //On focus event
-        $('.form-control').focus(function () {
-            $(this).parent().addClass('focused');
+        $parentSelector.find('.form-control').focus(function () {
+            $(this).closest('.form-line').addClass('focused');
         });
 
         //On focusout event
-        $('.form-control').focusout(function () {
+        $parentSelector.find('.form-control').focusout(function () {
             var $this = $(this);
             if ($this.parents('.form-group').hasClass('form-float')) {
                 if ($this.val() == '') { $this.parents('.form-line').removeClass('focused'); }
@@ -277,12 +288,12 @@ $.AdminBSB.input = {
         });
 
         //On label click
-        $('body').on('click', '.form-float .form-line .form-label', function () {
+        $parentSelector.on('click', '.form-float .form-line .form-label', function () {
             $(this).parent().find('input').focus();
         });
 
         //Not blank form
-        $('.form-control').each(function () {
+        $parentSelector.find('.form-control').each(function () {
             if ($(this).val() !== '') {
                 $(this).parents('.form-line').addClass('focused');
             }
