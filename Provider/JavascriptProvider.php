1c219fa3b9f976348adabeac691e48b0837a7ae1
webeweb <support@webeweb.fr>
2022-10-17 14:25:38 +0200

    Add Javascript provider

---
+++ Provider/JavascriptProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Provider;
+
+use WBW\Library\Symfony\Provider\JavascriptProviderInterface;
+
+/**
+ * Javascript provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\AdminBSBBundle\Provider
+ */
+class JavascriptProvider implements JavascriptProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.adminbsb.provider.javascript";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getJavascripts(): array {
+
+        return [
+            "wbwAdminBSBJQueryCountTo" => "@WBWAdminBSB/assets/wbwAdminBSBJQueryCountTo.js.twig",
+        ];
+    }
+}
---
+++ Tests/Provider/JavascriptProviderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the adminbsb-material-design-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\AdminBSBBundle\Tests\Provider;
+
+use WBW\Bundle\AdminBSBBundle\Provider\JavascriptProvider;
+use WBW\Bundle\AdminBSBBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Provider\JavascriptProviderInterface;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+
+/**
+ * Javascript provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\AdminBSBBundle\Tests\Provider
+ */
+class JavascriptProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests getJavascripts()
+     *
+     * @return void
+     */
+    public function testGetJavascript(): void {
+
+        $exp = [
+            "wbwAdminBSBJQueryCountTo"    => "@WBWAdminBSB/assets/wbwAdminBSBJQueryCountTo.js.twig",
+        ];
+
+        $obj = new JavascriptProvider();
+
+        $this->assertEquals($exp, $obj->getJavascripts());
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.adminbsb.provider.javascript", JavascriptProvider::SERVICE_NAME);
+
+        $obj = new JavascriptProvider();
+
+        $this->assertInstanceOf(ProviderInterface::class, $obj);
+        $this->assertInstanceOf(JavascriptProviderInterface::class, $obj);
+    }
+}
