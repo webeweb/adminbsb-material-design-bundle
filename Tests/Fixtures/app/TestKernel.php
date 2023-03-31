bb6ae59bf5b39cc1e5bc9fa8d460f287835c1399
webeweb <support@webeweb.fr>
2023-03-31 08:20:05 +0200

    Update test configuration

--- Tests/Fixtures/app/TestKernel.php
+++ Tests/Fixtures/app/TestKernel.php
@@ -9,6 +9,8 @@
  * file that was distributed with this source code.
  */
 
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 use WBW\Bundle\CoreBundle\Tests\AbstractKernel;
 
 /**
@@ -25,7 +27,6 @@ class TestKernel extends AbstractKernel {
 
         return [
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new FOS\UserBundle\FOSUserBundle,
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -35,5 +36,18 @@ class TestKernel extends AbstractKernel {
             new WBW\Bundle\CoreBundle\WBWCoreBundle(),
             new WBW\Bundle\JQuery\DataTablesBundle\WBWJQueryDataTablesBundle(),
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader): void {
+
+        if (6 <= Kernel::MAJOR_VERSION) {
+            parent::registerContainerConfiguration($loader);
+            return;
+        }
+
+        $loader->load(getcwd() . "/Tests/Fixtures/app/config/config_test.old.yml");
     }
 }
